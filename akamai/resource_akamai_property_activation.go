0416216ab2917f183a9ca74553fabef3191b2d81
Davey Shafik <dshafik@akamai.com>
2019-06-08 21:46:47 -0700

    Make activations function independently of properties, allow activating any specific version

--- akamai/resource_akamai_property_activation.go
+++ akamai/resource_akamai_property_activation.go
@@ -1,12 +1,13 @@
 package akamai
 
 import (
+	"encoding/json"
 	"errors"
+	"log"
+	"time"
+
 	"github.com/akamai/AkamaiOPEN-edgegrid-golang/papi-v1"
 	"github.com/hashicorp/terraform/helper/schema"
-	"log"
-	"strings"
-	"time"
 )
 
 func resourcePropertyActivation() *schema.Resource {
@@ -16,21 +17,18 @@ func resourcePropertyActivation() *schema.Resource {
 		Update: resourcePropertyActivationUpdate,
 		Delete: resourcePropertyActivationDelete,
 		Exists: resourcePropertyActivationExists,
-		Importer: &schema.ResourceImporter{
-			State: resourcePropertyActivationImport,
-		},
 		Schema: akamaiPropertyActivationSchema,
 	}
 }
 
 var akamaiPropertyActivationSchema = map[string]*schema.Schema{
-	"contract": &schema.Schema{
+	"property": &schema.Schema{
 		Type:     schema.TypeString,
-		Optional: true,
+		Required: true,
 		ForceNew: true,
 	},
-	"group": &schema.Schema{
-		Type:     schema.TypeString,
+	"version": &schema.Schema{
+		Type:     schema.TypeInt,
 		Optional: true,
 		ForceNew: true,
 	},
@@ -39,41 +37,18 @@ var akamaiPropertyActivationSchema = map[string]*schema.Schema{
 		Optional: true,
 		Default:  "staging",
 	},
-
 	"activate": &schema.Schema{
 		Type:     schema.TypeBool,
 		Optional: true,
 		Default:  true,
-	},
-	"name": &schema.Schema{
-		Type:     schema.TypeString,
-		Required: true,
-		ForceNew: true,
-	},
-	"hostname": &schema.Schema{
-		Type:     schema.TypeSet,
-		Required: true,
-		Elem:     &schema.Schema{Type: schema.TypeString},
 	},
 	"contact": &schema.Schema{
 		Type:     schema.TypeSet,
 		Required: true,
 		Elem:     &schema.Schema{Type: schema.TypeString},
 	},
-	"account": &schema.Schema{
+	"status": &schema.Schema{
 		Type:     schema.TypeString,
-		Computed: true,
-	},
-	"staging_version": &schema.Schema{
-		Type:     schema.TypeInt,
-		Computed: true,
-	},
-	"production_version": &schema.Schema{
-		Type:     schema.TypeInt,
-		Computed: true,
-	},
-	"version": &schema.Schema{
-		Type:     schema.TypeInt,
 		Computed: true,
 	},
 }
@@ -81,40 +56,15 @@ var akamaiPropertyActivationSchema = map[string]*schema.Schema{
 func resourcePropertyActivationCreate(d *schema.ResourceData, meta interface{}) error {
 	d.Partial(true)
 
-	group, e := getGroup(d)
-	if e != nil {
-		return e
-	}
-
-	contract, e := getContract(d)
-	if e != nil {
-		return e
-	}
-
-	var property *papi.Property
-	if property = findProperty(d); property == nil {
-		if group == nil {
-			return errors.New("group must be specified to activate a new property")
-		}
-
-		if contract == nil {
-			return errors.New("contract must be specified to activate a new property")
-		}
-
-	}
-
-	err := ensureEditableVersion(property)
+	property := papi.NewProperty(papi.NewProperties())
+	property.PropertyID = d.Get("property").(string)
+	err := property.GetProperty()
 	if err != nil {
-		return err
+		return errors.New("unable to find property")
 	}
-	d.Set("account", property.AccountID)
-	d.Set("version", property.LatestVersion)
 
 	// The API now has data, so save the partial state
 	d.SetId(property.PropertyID)
-	d.SetPartial("name")
-	d.SetPartial("contract")
-	d.SetPartial("group")
 	d.SetPartial("network")
 
 	if d.Get("activate").(bool) {
@@ -122,8 +72,8 @@ func resourcePropertyActivationCreate(d *schema.ResourceData, meta interface{}) 
 		if err != nil {
 			return err
 		}
-		d.SetPartial("contact")
 
+		d.Set("status", string(activation.Status))
 		go activation.PollStatus(property)
 
 	polling:
@@ -149,80 +99,53 @@ func resourcePropertyActivationCreate(d *schema.ResourceData, meta interface{}) 
 }
 
 func resourcePropertyActivationDelete(d *schema.ResourceData, meta interface{}) error {
-	log.Printf("[DEBUG] CANCEL ACTIVATION")
-
-	contractID, ok := d.GetOk("contract")
-	if !ok {
-		return errors.New("missing contract ID")
-	}
-	groupID, ok := d.GetOk("group")
-	if !ok {
-		return errors.New("missing group ID")
-	}
-	network, ok := d.GetOk("network")
-	if !ok {
-		return errors.New("missing network")
-	}
-
-	propertyID := d.Id()
+	log.Printf("[DEBUG] DEACTIVATE PROPERTY")
 
 	property := papi.NewProperty(papi.NewProperties())
-	property.PropertyID = propertyID
-	property.Contract = &papi.Contract{ContractID: contractID.(string)}
-	property.Group = &papi.Group{GroupID: groupID.(string)}
-
+	property.PropertyID = d.Get("property").(string)
 	e := property.GetProperty()
 	if e != nil {
 		return e
 	}
 
-	log.Printf("[DEBUG] CANCEL ACTIVATION PROPERTY %v", property)
+	log.Printf("[DEBUG] DEACTIVE PROPERTY %v", property)
 
 	activations, e := property.GetActivations()
 	if e != nil {
 		return e
 	}
-	log.Printf("[DEBUG] CANCEL ACTIVATION activations %v", activations)
-	activation, e := activations.GetLatestActivation(papi.NetworkValue(strings.ToUpper(network.(string))), papi.StatusActive)
-	log.Printf("[DEBUG] CANCEL ACTIVATION activations get latest %v", activations)
-	// an error here means there has not been any activation yet, so we can skip deactivating the property
-	// if there was no error, then activations were found, this can be an Activation or a Deactivation, so we check the ActivationType
-	// in case it has already been deactivated
-	if e == nil && activation.ActivationType == papi.ActivationTypeActivate {
-		log.Printf("[DEBUG] CANCEL ACTIVATION deactivations ")
-		deactivation := papi.NewActivation(papi.NewActivations())
-		deactivation.PropertyVersion = property.LatestVersion
-		deactivation.ActivationType = papi.ActivationTypeDeactivate
-		deactivation.Network = activation.Network
-		deactivation.NotifyEmails = activation.NotifyEmails
-		log.Printf("[DEBUG] CANCEL ACTIVATION deactivations %v", deactivation)
-		e = deactivation.Save(property, true)
-		if e != nil {
-			return e
-		}
-		log.Printf("[DEBUG] DEACTIVATION SAVED - ID %s STATUS %s\n", deactivation.ActivationID, deactivation.Status)
 
-		go deactivation.PollStatus(property)
+	network := d.Get("network").(papi.NetworkValue)
+	version := d.Get("version").(int)
+	for _, activation := range activations.Activations.Items {
+		if activation.Network == network && activation.PropertyVersion == version && activation.Status != papi.StatusInactive && activation.Status != papi.StatusDeactivated {
+			// The version is not inactive, so we need to deactivate it
+			activation, err := deactivateProperty(property, d, d.Get("network").(papi.NetworkValue))
+			if err != nil {
+				return err
+			}
 
-	polling:
-		for deactivation.Status != papi.StatusActive {
-			select {
-			case statusChanged := <-deactivation.StatusChange:
-				log.Printf("[DEBUG] Property Status: %s\n", deactivation.Status)
-				if statusChanged == false {
+			d.Set("status", string(activation.Status))
+			go activation.PollStatus(property)
+
+		polling:
+			//for activation.Status != papi.StatusActive {
+			for activation.Status != "Created" {
+				select {
+				case statusChanged := <-activation.StatusChange:
+					log.Printf("[DEBUG] Property Status: %s\n", activation.Status)
+					if statusChanged == false {
+						break polling
+					}
+					continue polling
+				case <-time.After(time.Minute * 90):
+					log.Println("[DEBUG] Activation Timeout (90 minutes)")
 					break polling
 				}
-				continue polling
-			case <-time.After(time.Minute * 90):
-				log.Println("[DEBUG] Deactivation Timeout (90 minutes)")
-				break polling
 			}
-		}
-	}
 
-	e = property.Delete()
-	if e != nil {
-		return e
+			d.Set("status", string(activation.Status))
+		}
 	}
 
 	d.SetId("")
@@ -232,72 +155,53 @@ func resourcePropertyActivationDelete(d *schema.ResourceData, meta interface{}) 
 	return nil
 }
 
-func resourcePropertyActivationImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
-	resourceID := d.Id()
-	propertyID := resourceID
+func resourcePropertyActivationExists(d *schema.ResourceData, meta interface{}) (bool, error) {
+	property := papi.NewProperty(papi.NewProperties())
+	property.PropertyID = d.Get("property").(string)
+	err := property.GetProperty()
+	if err != nil {
+		return false, err
+	}
 
-	if !strings.HasPrefix(resourceID, "prp_") {
-		for _, searchKey := range []papi.SearchKey{papi.SearchByPropertyName, papi.SearchByHostname, papi.SearchByEdgeHostname} {
-			results, err := papi.Search(searchKey, resourceID)
-			if err != nil {
-				continue
-			}
+	activations, err := property.GetActivations()
+	if err != nil {
+		// No activations found
+		return false, nil
+	}
 
-			if results != nil && len(results.Versions.Items) > 0 {
-				propertyID = results.Versions.Items[0].PropertyID
-				break
-			}
+	network := d.Get("network").(papi.NetworkValue)
+	version := d.Get("version").(int)
+	for _, activation := range activations.Activations.Items {
+		if activation.Network == network && activation.PropertyVersion == version {
+			return true, nil
 		}
 	}
 
-	property := papi.NewProperty(papi.NewProperties())
-	property.PropertyID = propertyID
-	e := property.GetProperty()
-	if e != nil {
-		return nil, e
-	}
-
-	d.Set("account", property.AccountID)
-	d.Set("contract", property.ContractID)
-	d.Set("group", property.GroupID)
-
-	d.Set("name", property.PropertyName)
-	d.Set("version", property.LatestVersion)
-	d.SetId(property.PropertyID)
-
-	return []*schema.ResourceData{d}, nil
-}
-
-func resourcePropertyActivationExists(d *schema.ResourceData, meta interface{}) (bool, error) {
-	property := papi.NewProperty(papi.NewProperties())
-	property.PropertyID = d.Id()
-	e := property.GetProperty()
-	if e != nil {
-		return false, e
-	}
-
-	return true, nil
+	return false, nil
 }
 
 func resourcePropertyActivationRead(d *schema.ResourceData, meta interface{}) error {
 	property := papi.NewProperty(papi.NewProperties())
-	property.PropertyID = d.Id()
+	property.PropertyID = d.Get("property").(string)
 	err := property.GetProperty()
 	if err != nil {
 		return err
 	}
 
-	d.Set("account", property.AccountID)
-	d.Set("contract", property.ContractID)
-	d.Set("group", property.GroupID)
-	d.Set("name", property.PropertyName)
-
-	d.Set("version", property.LatestVersion)
-	if property.StagingVersion > 0 {
-		d.Set("staging_version", property.StagingVersion)
+	d.SetId("")
+	activations, err := property.GetActivations()
+	if err != nil {
+		// No activations found
+		return nil
 	}
-	if property.ProductionVersion > 0 {
-		d.Set("production_version", property.ProductionVersion)
+
+	network := d.Get("network").(papi.NetworkValue)
+	version := d.Get("version").(int)
+	for _, activation := range activations.Activations.Items {
+		if activation.Network == network && activation.PropertyVersion == version {
+			d.SetId(activation.ActivationID)
+			d.Set("status", string(activation.Status))
+		}
 	}
 
 	return nil
@@ -305,30 +209,45 @@ func resourcePropertyActivationRead(d *schema.ResourceData, meta interface{}) er
 
 func resourcePropertyActivationUpdate(d *schema.ResourceData, meta interface{}) error {
 	log.Printf("[DEBUG] UPDATING")
-	d.Partial(true)
 
 	property, e := getProperty(d)
 	if e != nil {
 		return e
 	}
 
-	err := ensureEditableVersion(property)
-	if err != nil {
-		return err
-	}
-	d.Set("version", property.LatestVersion)
-
-	d.SetPartial("default")
-	d.SetPartial("origin")
-
-	// an existing activation on this property will be automatically deactivated upon
-	// creation of this new activation
 	if d.Get("activate").(bool) {
-		activation, err := activateProperty(property, d)
+		activations, err := property.GetActivations()
 		if err != nil {
-			return err
+			// No activations found
+			return nil
 		}
-		d.SetPartial("contact")
+
+		old, new := d.GetChange("network")
+		if old.(papi.NetworkValue) != new.(papi.NetworkValue) {
+			// deactivate on the old network, we don't need to wait for this
+			deactivateProperty(property, d, old.(papi.NetworkValue))
+		}
+
+		var activation *papi.Activation
+		network := d.Get("network").(papi.NetworkValue)
+		version := d.Get("version").(int)
+		for _, a := range activations.Activations.Items {
+			if a.Network == network && a.PropertyVersion == version && a.Status != papi.StatusFailed && a.Status != papi.StatusDeactivated && a.Status != papi.StatusAborted && a.ActivationType != papi.ActivationTypeDeactivate {
+				activation = a
+				break
+			}
+		}
+
+		// Already an activation in process, we'll just wait on that one
+		if activation == nil {
+			activation, err = activateProperty(property, d)
+			if err != nil {
+				return err
+			}
+		}
+
+		d.SetId(activation.ActivationID)
+		d.Set("status", string(activation.Status))
 
 		go activation.PollStatus(property)
 
@@ -346,49 +265,53 @@ func resourcePropertyActivationUpdate(d *schema.ResourceData, meta interface{}) 
 				break polling
 			}
 		}
+		d.Set("status", string(activation.Status))
+	} else {
+		return resourcePropertyRead(d, meta)
 	}
-
-	d.Partial(false)
 
 	log.Println("[DEBUG] Done")
 	return nil
 }
 
-// Helpers
-
-func findPropertyActivation(d *schema.ResourceData) (*papi.Property, error) {
-	results, err := papi.Search(papi.SearchByPropertyName, d.Get("name").(string))
+func activateProperty(property *papi.Property, d *schema.ResourceData) (*papi.Activation, error) {
+	activation := getActivation(d, papi.ActivationTypeActivate, d.Get("network").(papi.NetworkValue))
+	err := activation.Save(property, true)
 	if err != nil {
+		body, _ := json.Marshal(activation)
+		log.Printf("[DEBUG] API Request Body: %s\n", string(body))
 		return nil, err
 	}
+	log.Println("[DEBUG] Activation submitted successfully")
 
-	if results == nil || len(results.Versions.Items) == 0 {
-		for _, hostname := range d.Get("hostname").(*schema.Set).List() {
-			results, err = papi.Search(papi.SearchByHostname, hostname.(string))
-			if err == nil && (results == nil || len(results.Versions.Items) != 0) {
-				break
-			}
-		}
+	return activation, nil
+}
 
-		if err != nil || (results == nil || len(results.Versions.Items) == 0) {
-			return nil, err
-		}
-	}
-
-	property := &papi.Property{
-		PropertyID: results.Versions.Items[0].PropertyID,
-		Group: &papi.Group{
-			GroupID: results.Versions.Items[0].GroupID,
-		},
-		Contract: &papi.Contract{
-			ContractID: results.Versions.Items[0].ContractID,
-		},
-	}
-
-	err = property.GetProperty()
+func deactivateProperty(property *papi.Property, d *schema.ResourceData, network papi.NetworkValue) (*papi.Activation, error) {
+	activation := getActivation(d, papi.ActivationTypeDeactivate, network)
+	err := activation.Save(property, true)
 	if err != nil {
+		body, _ := json.Marshal(activation)
+		log.Printf("[DEBUG] API Request Body: %s\n", string(body))
 		return nil, err
 	}
+	log.Println("[DEBUG] Deactivation submitted successfully")
 
-	return property, err
+	return activation, nil
+}
+
+func getActivation(d *schema.ResourceData, activationType papi.ActivationValue, network papi.NetworkValue) *papi.Activation {
+	log.Println("[DEBUG] Creating new activation")
+	activation := papi.NewActivation(papi.NewActivations())
+	activation.PropertyVersion = d.Get("version").(int)
+	activation.Network = network
+	for _, email := range d.Get("contact").(*schema.Set).List() {
+		activation.NotifyEmails = append(activation.NotifyEmails, email.(string))
+	}
+	activation.Note = "Using Terraform"
+
+	activation.ActivationType = activationType
+
+	log.Println("[DEBUG] Activating")
+	return activation
 }
--- akamai/resource_akamai_property_activation_test.go
+++ akamai/resource_akamai_property_activation_test.go
@@ -1,53 +1,142 @@
 package akamai
 
 import (
-    "fmt"
-    "github.com/akamai/AkamaiOPEN-edgegrid-golang/papi-v1"
+	"fmt"
+	"log"
+	"strings"
+	"testing"
+
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/papi-v1"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
-    "log"
-    "strings"
-	"testing"
 )
 
 var testAccAkamaiPropertyActivationConfig = fmt.Sprintf(`
 provider "akamai" {
-    edgerc = "~/.edgerc"
-    papi_section = "papi_section"
+  edgerc = "~/.edgerc"
+  papi_section = "papi"
 }
 
-variable "activate" {
-   default = true
+resource "akamai_property_activation" "property_activation" {
+	property = "${akamai_property.akamai_developer.id}"
+	version = "${akamai_property.akamai_developer.version}"
+	network = "STAGING"
+	activate = true
+	contact = ["user@example.org"]
 }
 
-resource "akamai_property_activation" "dshafik_sandbox" {
-
-       name = "akavadeveloper.com"
-       contact = ["martin@akava.io"]
-       hostname = ["akavadeveloper.com"]
-       contract = "${data.akamai_contract.our_contract.id}"
-       group =  "${data.akamai_group.our_group.id}"
-       network = "STAGING"
-       activate = "${var.activate}"
+data "akamai_contract" "contract" {
 }
 
-data "akamai_group" "our_group" {
-   name = "Davey Shafik"
+data "akamai_group" "group" {
 }
 
-output "groupid" {
- value = "${data.akamai_group.our_group.id}"
+resource "akamai_cp_code" "cp_code" {
+	name = "terraform-testing"
+	contract = "${akamai_contract.contract.id}"
+	group = "${akamai_group.group.id}"
+	product = "prd_SPM"
 }
 
-
-data "akamai_contract" "our_contract" {
-   name = "Davey Shafik"
+resource "random_pet" "property_name" {
 }
 
-output "contractid" {
- value = "${data.akamai_contract.our_contract.id}"
+resource "akamai_property" "akamai_developer" {
+  name = "${random_pet.property_name.id}"
+
+  contact = ["user@example.org"]
+
+  product = "prd_SPM"
+  cp_code = "${akamai_cp_code.cp_code.id}"
+  contract = "${data.akamai_contract.contract.id}"
+  group = "${data.akamai_group.group.id}"
+  
+  rule_format = "v2016-11-15"
+  
+  rules = "${akamai_property_rules.rules.json}"
 }
 
+resource "akamai_property_rules" "rules" {
+ 	rules {
+		behavior {
+			name = "origin"
+        	option { 
+       			name = "cacheKeyHostname"
+            	value = "ORIGIN_HOSTNAME"
+        	}
+			option { 
+    			name = "compress"
+     			value = true
+     		}
+    		option { 
+    			name = "enableTrueClientIp"
+     			value = false
+     		}
+    		option { 
+    			name = "forwardHostHeader"
+     			value = "REQUEST_HOST_HEADER"
+     		}
+    		option { 
+    			name = "hostname"
+     			value = "example.org"
+     		}
+    		option { 
+    			name = "httpPort"
+     			value = 80
+     		}
+    		option { 
+    			name = "httpsPort"
+     			value = 443
+     		}
+    		option { 
+    			name = "originSni"
+     			value = true
+     		}
+    		option { 
+    			name = "originType"
+     			value = "CUSTOMER"
+     		}
+    		option { 
+    			name = "verificationMode"
+     			value = "PLATFORM_SETTINGS"
+     		}
+    		option { 
+    			name = "originCertificate"
+     			value = ""
+     		}
+    		option { 
+    			name = "ports"
+     			value = ""
+     		}
+      	}
+		behavior {
+			name = "cpCode"
+			option {
+				name = "id"
+				value = "${akamai_cp_code.cp_code.id}"
+			}
+			option {
+				name = "name"
+				value = "${akamai_cp_code.cp_code.name}"
+			}
+		}
+		behavior {
+			name = "caching"
+			option {
+				name = "behavior"
+				value = "MAX_AGE"
+			}
+			option {
+                name = "mustRevalidate"
+                value = "false"
+			}
+            option {
+                name = "ttl"
+                value = "1d"
+            }
+		}
+    }
+}
 `)
 
 func TestAccAkamaiPropertyActivation_basic(t *testing.T) {
@@ -71,9 +160,9 @@ func testAccCheckAkamaiPropertyActivationDestroy(s *terraform.State) error {
 		if rs.Type != "akamai_property_activation" {
 			continue
 		}
-				
+
 		log.Printf("[DEBUG] [Akamai PropertyActivation] Activation Delete")
-				
+
 	}
 	return nil
 }
@@ -83,30 +172,28 @@ func testAccCheckAkamaiPropertyActivationExists(s *terraform.State) error {
 		if rs.Type != "akamai_property_activation" {
 			continue
 		}
+		propertyID := rs.Primary.ID
 
-		
-	propertyID := rs.Primary.ID
+		property := papi.NewProperty(papi.NewProperties())
+		property.PropertyID = propertyID
+		property.Contract = &papi.Contract{ContractID: rs.Primary.Attributes["contract"]}
+		property.Group = &papi.Group{GroupID: rs.Primary.Attributes["group"]}
 
-	property := papi.NewProperty(papi.NewProperties())
-	property.PropertyID = propertyID
-	property.Contract = &papi.Contract{ContractID: rs.Primary.Attributes["contract"]}
-	property.Group = &papi.Group{GroupID: rs.Primary.Attributes["group"]}
+		e := property.GetProperty()
+		if e != nil {
+			return e
+		}
 
-	e := property.GetProperty()
-	if e != nil {
-		return e
-	}
+		log.Printf("[DEBUG] GET ACTIVATION PROPERTY %v", property)
 
-	log.Printf("[DEBUG] GET ACTIVATION PROPERTY %v", property)
-
-	activations, e := property.GetActivations()
-	if e != nil {
-		return e
-	}
-	log.Printf("[DEBUG] ACTIVATION activations %v", activations)
-	activation, e := activations.GetLatestActivation(papi.NetworkValue(strings.ToUpper(rs.Primary.Attributes["network"])), papi.StatusActive)
-	log.Printf("[DEBUG] ACTIVATION activations get latest %v", activations)
-    log.Printf("[DEBUG] ACTIVATION activation get latest %v", activation)
+		activations, e := property.GetActivations()
+		if e != nil {
+			return e
+		}
+		log.Printf("[DEBUG] ACTIVATION activations %v", activations)
+		activation, e := activations.GetLatestActivation(papi.NetworkValue(strings.ToUpper(rs.Primary.Attributes["network"])), papi.StatusActive)
+		log.Printf("[DEBUG] ACTIVATION activations get latest %v", activations)
+		log.Printf("[DEBUG] ACTIVATION activation get latest %v", activation)
 	}
 	return nil
 }
