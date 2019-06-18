be83c26446bba82db5a3fb4dafa2a9c6b7c2d634
Davey Shafik <dshafik@akamai.com>
2019-06-18 11:18:30 -0700

    JSON ordering is non-deterministic, just check it's set

--- akamai/resource_akamai_property_variables_test.go
+++ akamai/resource_akamai_property_variables_test.go
@@ -28,7 +28,6 @@ resource "akamai_property_variables" "test" {
 `)
 
 func TestAccAkamaiPropertyVariables_basic(t *testing.T) {
-	json := `{"accountId":"","contractId":"","groupId":"","propertyId":"","propertyVersion":0,"etag":"","ruleFormat":"","rules":{"name":"default","behaviors":[{"name":"caching","options":{"behavior":"MAX_AGE","mustRevalidate":false,"ttl":"1d"}},{"name":"cpCode","options":{"id":"cp-code-id"}},{"name":"origin","options":{"cacheKeyHostname":"ORIGIN_HOSTNAME","compress":1,"enableTrueClientIp":0,"forwardHostHeader":"REQUEST_HOST_HEADER","hostname":"exampleterraform.io","httpPort":80,"httpsPort":443,"originCertificate":"","originSni":1,"originType":"CUSTOMER","ports":"","verificationMode":"PLATFORM_SETTINGS"}}],"options":{}}}`
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
@@ -37,8 +36,8 @@ func TestAccAkamaiPropertyVariables_basic(t *testing.T) {
 			{
 				Config: testAccAkamaiPropertyVariablesConfig,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"akamai_property_variables.test", "json", json,
+					resource.TestCheckResourceAttrSet(
+						"akamai_property_variables.test", "json",
 					),
 				),
 			},
