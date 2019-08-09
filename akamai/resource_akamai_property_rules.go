8fbe0f1cf9f01867050899083cbbece8ac923ac6
Davey Shafik <dshafik@akamai.com>
2019-08-09 16:30:05 -0700

    Change `akamai_property_rules` to a data source, resource `akamai_property_rules` will now error
    - Fixed setting of hostnames (Fixes #44)
    - Fixed updating of rules (Fixes #30)

--- akamai/resource_akamai_property_rules.go
+++ akamai/resource_akamai_property_rules.go
@@ -1,12 +1,8 @@
 package akamai
 
 import (
-	"github.com/akamai/AkamaiOPEN-edgegrid-golang/jsonhooks-v1"
-	"github.com/akamai/AkamaiOPEN-edgegrid-golang/papi-v1"
+	"errors"
 	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/tidwall/gjson"
-	"log"
-	"strings"
 )
 
 func resourcePropertyRules() *schema.Resource {
@@ -16,61 +12,8 @@ func resourcePropertyRules() *schema.Resource {
 		Update: resourcePropertyRulesUpdate,
 		Delete: resourcePropertyRulesDelete,
 		Exists: resourcePropertyRulesExists,
-		Importer: &schema.ResourceImporter{
-			State: resourcePropertyImport,
-		},
 		Schema: akamaiPropertyRulesSchema,
 	}
-}
-
-var akpsRulesOption = &schema.Schema{
-	Type:     schema.TypeSet,
-	Optional: true,
-	Elem: &schema.Resource{
-		Schema: map[string]*schema.Schema{
-			"key": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"values": {
-				Type:     schema.TypeSet,
-				Elem:     &schema.Schema{Type: schema.TypeString},
-				Optional: true,
-			},
-			"value": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-		},
-	},
-}
-
-var akpsRulesCriteria = &schema.Schema{
-	Type:     schema.TypeSet,
-	Optional: true,
-	Elem: &schema.Resource{
-		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"option": akpsRulesOption,
-		},
-	},
-}
-
-var akpsRulesBehavior = &schema.Schema{
-	Type:     schema.TypeSet,
-	Optional: true,
-	Elem: &schema.Resource{
-		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"option": akpsRulesOption,
-		},
-	},
 }
 
 var akamaiPropertyRulesSchema = map[string]*schema.Schema{
@@ -227,200 +170,21 @@ var akamaiPropertyRulesSchema = map[string]*schema.Schema{
 }
 
 func resourcePropertyRulesCreate(d *schema.ResourceData, meta interface{}) error {
-
-	rules := papi.NewRules()
-
-	// get rules from the TF config
-	unmarshalRules(d, rules)
-
-	jsonBody, err := jsonhooks.Marshal(rules)
-	if err != nil {
-		return err
-	}
-
-	sha := getSHAString(string(jsonBody))
-	d.Set("json", string(jsonBody))
-
-	d.SetId(sha)
-
-	log.Println("[DEBUG] Done")
-	return nil
+	return errors.New("The akamai_property_rules resource has moved to a data source, please change 'resource \"akamai_property_rules\"' to 'data \"akamai_property_rules\"")
 }
 
 func resourcePropertyRulesDelete(d *schema.ResourceData, meta interface{}) error {
-	log.Printf("[DEBUG] DELETING")
-	d.SetId("")
-
-	log.Println("[DEBUG] Done")
-
-	return nil
-}
-
-func resourcePropertyRulesImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
-	resourceID := d.Id()
-	propertyID := resourceID
-
-	if !strings.HasPrefix(resourceID, "prp_") {
-		for _, searchKey := range []papi.SearchKey{papi.SearchByPropertyName, papi.SearchByHostname, papi.SearchByEdgeHostname} {
-			results, err := papi.Search(searchKey, resourceID)
-			if err != nil {
-				continue
-			}
-
-			if results != nil && len(results.Versions.Items) > 0 {
-				propertyID = results.Versions.Items[0].PropertyID
-				break
-			}
-		}
-	}
-
-	property := papi.NewProperty(papi.NewProperties())
-	property.PropertyID = propertyID
-	e := property.GetProperty()
-	if e != nil {
-		return nil, e
-	}
-
-	d.Set("account", property.AccountID)
-	d.Set("contract", property.ContractID)
-	d.Set("group", property.GroupID)
-
-	d.Set("name", property.PropertyName)
-	d.Set("version", property.LatestVersion)
-	d.SetId(property.PropertyID)
-
-	return []*schema.ResourceData{d}, nil
+	return errors.New("The akamai_property_rules resource has moved to a data source, please change 'resource \"akamai_property_rules\"' to 'data \"akamai_property_rules\"")
 }
 
 func resourcePropertyRulesExists(d *schema.ResourceData, meta interface{}) (bool, error) {
-	return true, nil
+	return false, errors.New("The akamai_property_rules resource has moved to a data source, please change 'resource \"akamai_property_rules\"' to 'data \"akamai_property_rules\"")
 }
 
 func resourcePropertyRulesRead(d *schema.ResourceData, meta interface{}) error {
-
-	log.Println("[DEBUG] READ" + d.Id())
-
-	return nil
+	return errors.New("The akamai_property_rules resource has moved to a data source, please change 'resource \"akamai_property_rules\"' to 'data \"akamai_property_rules\"")
 }
 
 func resourcePropertyRulesUpdate(d *schema.ResourceData, meta interface{}) error {
-	log.Printf("[DEBUG] UPDATING")
-	d.Partial(true)
-	rules := papi.NewRules()
-
-	// get rules from the TF config
-	unmarshalRules(d, rules)
-
-	jsonBody, err := jsonhooks.Marshal(rules)
-	if err != nil {
-		return err
-	}
-
-	sha := getSHAString(string(jsonBody))
-	d.Set("json", string(jsonBody))
-	//d.Set("json", jsonBody)
-	d.SetId(sha)
-	d.Partial(false)
-
-	log.Println("[DEBUG] Done")
-	return nil
-}
-
-func unmarshalRules(d *schema.ResourceData, propertyRules *papi.Rules) {
-	// Default Rules
-	rules, ok := d.GetOk("rules")
-	if ok {
-		for _, r := range rules.(*schema.Set).List() {
-			ruleTree, ok := r.(map[string]interface{})
-			if ok {
-				behavior, ok := ruleTree["behavior"]
-				if ok {
-					for _, b := range behavior.(*schema.Set).List() {
-						bb, ok := b.(map[string]interface{})
-						if ok {
-							beh := papi.NewBehavior()
-							beh.Name = bb["name"].(string)
-							boptions, ok := bb["option"]
-							if ok {
-								beh.Options = extractOptions(boptions.(*schema.Set))
-							}
-
-							// Fixup CPCode
-							if beh.Name == "cpCode" {
-								if _, ok := beh.Options["value"]; !ok {
-									if _, ok := beh.Options["id"]; ok {
-										cpCode := papi.NewCpCodes(nil, nil).NewCpCode()
-										cpCode.CpcodeID = beh.Options["id"].(string)
-										beh.Options = papi.OptionValue{"value": papi.OptionValue{"id": cpCode.ID()}}
-									}
-								}
-							}
-
-							// Fixup SiteShield
-							if beh.Name == "siteShield" {
-								if _, ok := beh.Options["ssmap"].(string); ok {
-									beh.Options = papi.OptionValue{"ssmap": papi.OptionValue{"value": beh.Options["ssmap"].(string)}}
-								}
-							}
-
-							propertyRules.Rule.MergeBehavior(beh)
-						}
-					}
-				}
-
-				criteria, ok := ruleTree["criteria"]
-				if ok {
-					for _, c := range criteria.(*schema.Set).List() {
-						cc, ok := c.(map[string]interface{})
-						if ok {
-							newCriteria := papi.NewCriteria()
-							newCriteria.Name = cc["name"].(string)
-							coptions, ok := cc["option"]
-							if ok {
-								newCriteria.Options = extractOptions(coptions.(*schema.Set))
-							}
-							propertyRules.Rule.MergeCriteria(newCriteria)
-						}
-					}
-				}
-
-				isSecure, ok := ruleTree["is_secure"].(bool)
-				if ok && isSecure {
-					propertyRules.Rule.Options.IsSecure = isSecure
-				}
-			}
-
-			childRules, ok := ruleTree["rule"]
-			if ok {
-				for _, rule := range extractRules(childRules.(*schema.Set)) {
-					propertyRules.Rule.MergeChildRule(rule)
-				}
-			}
-		}
-
-		// ADD vars from variables resource
-		jsonvars, ok := d.GetOk("variables")
-		if ok {
-			log.Println("VARS from JSON ", jsonvars)
-			variables := gjson.Parse(jsonvars.(string))
-			result := gjson.Get(variables.String(), "variables") //gjson.GetMany(variables.String(),"variables.#.name","variables.#.description","variables.#.value","variables.#.hidden","variables.#.sensitive" )
-
-			result.ForEach(func(key, value gjson.Result) bool {
-				variableMap, ok := value.Value().(map[string]interface{})
-				log.Println("VARS from JSON LOOP NAME ", variableMap["name"].(string))
-				log.Println("VARS from JSON LOOP DESC ", variableMap["description"].(string))
-				if ok {
-					newVariable := papi.NewVariable()
-					newVariable.Name = variableMap["name"].(string)
-					newVariable.Description = variableMap["description"].(string)
-					newVariable.Value = variableMap["value"].(string)
-					newVariable.Hidden = variableMap["hidden"].(bool)
-					newVariable.Sensitive = variableMap["sensitive"].(bool)
-					propertyRules.Rule.AddVariable(newVariable)
-				}
-
-				return true
-			}) //variables
-		}
-	}
+	return errors.New("The akamai_property_rules resource has moved to a data source, please change 'resource \"akamai_property_rules\"' to 'data \"akamai_property_rules\"")
 }
