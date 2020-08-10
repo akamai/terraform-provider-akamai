82b6b17bd25ad12c5b37d4341fa50c9c0b6967d4
Narra, Vinnuth <vnarra@akamai.com>
2020-08-09 23:33:59 -0400

    TFP-9 Add Dockerfile and local testing script
    local vendored environment with docker
    
    log env variables to docker run
    
    Debug logs and no cache local build
    
    minor tweaks
    
    TFP-9 added pom to integrate into akamai CI process
    
    Updated goreleaser to support freebsd.  Added semtag script for easier version tag updates
    
    Moved docker files to go build directory; moved scripts to scripts
    
    cleanup docker scripts

--- examples/akamai_property/property-json/property.tf
+++ examples/akamai_property/property-json/property.tf
@@ -1,6 +1,18 @@
+terraform {
+  required_providers {
+    akamai = {
+      source = "akamai/akamai"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
 provider "akamai" {
   edgerc = "~/.edgerc"
-  papi_section = "global"
+  config_section = "global"
 }
 
 resource "akamai_property" "terraform_example" {
@@ -9,33 +21,33 @@ resource "akamai_property" "terraform_example" {
     "martin@akava.io"
   ]
   product = "prd_SPM"
-  contract = "${data.akamai_contract.contract.id}"
-  group = "${data.akamai_group.group.id}"
+  contract = data.akamai_contract.contract.id
+  group = data.akamai_group.group.id
   cp_code = "cpc_846642"
 
   hostnames = {
-    "terraform.example1.org" = "${akamai_edge_hostname.ehn.edge_hostname}"
-    "terraform.example1.com" = "${akamai_edge_hostname.ehn.edge_hostname}"
+    "terraform.example1.org" = akamai_edge_hostname.ehn.edge_hostname
+    "terraform.example1.com" = akamai_edge_hostname.ehn.edge_hostname
   }
 
   rule_format = "v2019-07-25"
-  variables = "${akamai_property_variables.origin.json}"
-  rules = "${data.local_file.rules.content}"
+  variables = akamai_property_variables.origin.json
+  rules = data.local_file.rules.content
 }
 
 resource "akamai_edge_hostname" "ehn" {
   edge_hostname = "terraform.example1.org.edgesuite.net"
 
   product = "prd_SPM"
-  contract = "${data.akamai_contract.contract.id}"
-  group = "${data.akamai_group.group.id}"
+  contract = data.akamai_contract.contract.id
+  group = data.akamai_group.group.id
 
   ipv4 = true
   ipv6 = true
 }
 
 data "akamai_contract" "contract" {
-  group = "${data.akamai_group.group.name}"
+  group = data.akamai_group.group.name
 }
 
 data "akamai_group" "group" {
