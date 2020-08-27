54c32cdc3d3c1fdef510e6b830739dd1911f210b
López López, Roberto <rlopezlo@akamai.com>
2020-08-27 13:53:38 +0200

    [TFP-38] some safe casting

--- pkg/providers/gtm/data_akamai_gtm_default_datacenter.go
+++ pkg/providers/gtm/data_akamai_gtm_default_datacenter.go
@@ -37,9 +37,13 @@ func dataSourceGTMDefaultDatacenter() *schema.Resource {
 
 // validateDCValue is a SchemaValidateFunc to validate the DC value.
 func validateDCValue(v interface{}, k string) (ws []string, es []error) {
-	value := v.(int)
+	value, ok := v.(int)
+	if !ok {
+		es = append(es, fmt.Errorf("wrong cast"))
+		return
+	}
 	if value != gtm.MapDefaultDC && value != gtm.Ipv4DefaultDC && value != gtm.Ipv6DefaultDC {
-		es = append(es, fmt.Errorf("Datacenter value must be %d, %d, or %d", gtm.MapDefaultDC, gtm.Ipv4DefaultDC, gtm.Ipv6DefaultDC))
+		es = append(es, fmt.Errorf("datacenter value must be %d, %d, or %d", gtm.MapDefaultDC, gtm.Ipv4DefaultDC, gtm.Ipv6DefaultDC))
 	}
 	return
 }
@@ -52,9 +56,13 @@ func dataSourceGTMDefaultDatacenterRead(d *schema.ResourceData, meta interface{}
 		return fmt.Errorf("[Error] GTM dataSourceGTMDefaultDatacenterRead: Domain not initialized")
 	}
 	// get or create default dc
+	dcid, ok := d.Get("datacenter").(int)
+	if !ok {
+		return fmt.Errorf("[Error] GTM dataSourceGTMDefaultDatacenterRead: datacenter not initialized")
+	}
+
+	var defaultDC = gtm.NewDatacenter()
 	var err error
-	dcid := d.Get("datacenter").(int)
-	defaultDC := gtm.NewDatacenter()
 	if dcid == gtm.MapDefaultDC {
 		defaultDC, err = gtm.CreateMapsDefaultDatacenter(domain.(string))
 	} else if dcid == gtm.Ipv4DefaultDC {
@@ -71,8 +79,8 @@ func dataSourceGTMDefaultDatacenterRead(d *schema.ResourceData, meta interface{}
 	if defaultDC == nil {
 		return fmt.Errorf("[Error] GTM dataSourceGTMDefaultDatacenterRead: Default Datacenter does not Exist")
 	}
-	d.Set("nickname", defaultDC.Nickname)
-	d.Set("datacenter_id", defaultDC.DatacenterId)
+	_ = d.Set("nickname", defaultDC.Nickname)
+	_ = d.Set("datacenter_id", defaultDC.DatacenterId)
 	defaultDatacenterId := fmt.Sprintf("%s:%s:%d", domain.(string), "default_datcenter", defaultDC.DatacenterId)
 	log.Printf("[DEBUG] [Akamai GTMv1] Generated Default DC Resource Id: %s", defaultDatacenterId)
 	d.SetId(defaultDatacenterId)
