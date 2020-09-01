ce15febd68468f0716a4abf620080a5a3ca40d80
Edward Lynes <elynes@akamai.com>
2020-09-01 09:15:54 -0400

    v2 conversion

--- pkg/providers/dns/data_dns_record_set.go
+++ pkg/providers/dns/data_dns_record_set.go
@@ -1,17 +1,17 @@
 package dns
 
 import (
-	"fmt"
-	"log"
+	"context"
 	"sort"
-
+	"github.com/akamai/terraform-provider-akamai/v2/pkg/akamai"
 	dnsv2 "github.com/akamai/AkamaiOPEN-edgegrid-golang/configdns-v2"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 )
 
 func dataSourceDNSRecordSet() *schema.Resource {
 	return &schema.Resource{
-		Read: dataSourceDNSRecordSetRead,
+		ReadContext: dataSourceDNSRecordSetRead,
 		Schema: map[string]*schema.Schema{
 			"zone": {
 				Type:     schema.TypeString,
@@ -34,24 +34,29 @@ func dataSourceDNSRecordSet() *schema.Resource {
 	}
 }
 
-func dataSourceDNSRecordSetRead(d *schema.ResourceData, meta interface{}) error {
+func dataSourceDNSRecordSetRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+
+	akactx := akamai.ContextGet(inst.Name())
 	zone := d.Get("zone").(string)
 	host := d.Get("host").(string)
 	recordtype := d.Get("record_type").(string)
 
-	log.Printf("[DEBUG] [Akamai DNSv2] Start Searching for records %s %s %s ", zone, host, recordtype)
+	log := akactx.Log("DNS", "dataSourceDNSRecordSetRead")
+	//CorrelationID := "[DNS][dataSourceDNSRecordSetRead-" + akactx.OperationID() + "]"
+
+	log.Debug("[Akamai DNSv2] Start Searching for records %s %s %s ", zone, host, recordtype)
+	// Warning or Errors can be collected in a slice type
+	var diags diag.Diagnostics
 
 	rdata, err := dnsv2.GetRdata(zone, host, recordtype)
 	if err != nil {
-		return fmt.Errorf("error looking up A records for %q: %s", host, err)
+		diags = append(diags, diag.Errorf("error looking up A records for %q: %s", host, err)...)
+	} else {
+		log.Debug("[Akamai DNSv2] Searching for records [%v]", rdata)
+		sort.Strings(rdata)
+
+		d.Set("rdata", rdata)
+		d.SetId(host)
 	}
-
-	log.Printf("[DEBUG] [Akamai DNSv2] Searching for records [%v]", rdata)
-
-	sort.Strings(rdata)
-
-	d.Set("rdata", rdata)
-	d.SetId(host)
-
-	return nil
+	return diags
 }
