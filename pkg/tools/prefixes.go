269fff8bf05dac9e2dfdfee01466c2968487e6cf
Narra, Vinnuth <vnarra@akamai.com>
2020-09-18 10:18:07 -0400

    err msg update

--- pkg/tools/prefixes.go
+++ pkg/tools/prefixes.go
@@ -1,16 +1,17 @@
 package tools
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 )
 
 func AddPrefix(str string, pre string) (string, error) {
 	if len(strings.TrimSpace(str)) == 0 {
-		return "", fmt.Errorf("%w: %s", ErrEmptyKey, str)
+		return "", fmt.Errorf("%w: %s", errors.New("Prefix string cannot be blank"), str)
 	}
 	if len(strings.TrimSpace(pre)) == 0 {
-		return "", fmt.Errorf("%w: %s", ErrEmptyKey, pre)
+		return "", fmt.Errorf("%w: %s", errors.New("Prefix cannot be blank"), pre)
 	}
 	if strings.HasPrefix(str, pre) {
 		return str, nil
