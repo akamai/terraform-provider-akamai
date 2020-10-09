e80524d7552748a01234bc69edb411290356b417
St. Lawrence, Zachary <zstlawre@akamai.com>
2020-10-09 10:42:24 -0400

    TFP-259 add unit test for cpcode changes

--- pkg/providers/property/resource_akamai_cp_code.go
+++ pkg/providers/property/resource_akamai_cp_code.go
@@ -140,6 +140,9 @@ func resourceCPCodeRead(ctx context.Context, d *schema.ResourceData, m interface
 		return diag.FromErr(fmt.Errorf("%w: %s", tools.ErrValueSet, err.Error()))
 	}
 	// we use the first value returned.  Most cpcodes have but a single product and we need to pick one for comparison.
+	if len(cpCode.ProductIDs) == 0 {
+		return diag.Errorf("Couldn't find product id on the CP Code")
+	}
 	if err := d.Set("product", cpCode.ProductIDs[0]); err != nil {
 		return diag.FromErr(fmt.Errorf("%w: %s", tools.ErrValueSet, err.Error()))
 	}
