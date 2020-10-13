7828d5edea929879fb647ef76ced4978f4852bac
Rob Rodriguez <rrodrigu@akamai.com>
2020-10-13 10:32:16 -0700

    fixing break

--- pkg/providers/dns/resource_akamai_dns_record_test.go
+++ pkg/providers/dns/resource_akamai_dns_record_test.go
@@ -67,7 +67,8 @@ func TestResDnsRecord(t *testing.T) {
 
 			rec.Target = []string{"10.0.0.4", "10.0.0.5"}
 
-			parseCall.ReturnArguments = mock.Arguments{parseRData(context.Background(), "A", rec.Target)}
+			parsedData = parseRData(context.Background(), "A", rec.Target)
+			parseCall.ReturnArguments = mock.Arguments{parsedData}
 		})
 
 		client.On("DeleteRecord",
@@ -106,269 +107,253 @@ func TestResDnsRecord(t *testing.T) {
 
 		client.AssertExpectations(t)
 	})
-	/*
-		// This example tests attempting to create an A record that already exists on the server
-		// It is not a full lifecycle test as the "update" occurs in the create method
-		t.Run("update existing record", func(t *testing.T) {
-			client := &mockdns{}
 
-			client.On("GetRecord",
-				mock.Anything, // ctx is irrelevant for this test
-				"exampleterraform.io",
-				"exampleterraform.io",
-				"A",
-			).Return(rec, nil)
+	// This example tests attempting to create an A record that already exists on the server
+	// It is not a full lifecycle test as the "update" occurs in the create method
+	t.Run("update existing record", func(t *testing.T) {
+		client := &mockdns{}
 
-			client.On("ProcessRdata",
-				mock.Anything, // ctx is irrelevant for this test
-				mock.AnythingOfType("[]string"),
-				"A",
-			).Return(rec.Target, nil)
+		client.On("GetRecord",
+			mock.Anything, // ctx is irrelevant for this test
+			"exampleterraform.io",
+			"exampleterraform.io",
+			"A",
+		).Return(rec, nil)
 
-			client.On("UpdateRecord",
-				mock.Anything, // ctx is irrelevant for this test
-				mock.AnythingOfType("*dns.RecordBody"),
-				"exampleterraform.io",
-				mock.Anything,
-			).Return(nil)
+		client.On("ProcessRdata",
+			mock.Anything, // ctx is irrelevant for this test
+			mock.AnythingOfType("[]string"),
+			"A",
+		).Return(rec.Target, nil)
 
-			client.On("ParseRData",
-				mock.Anything,
-				"A",
-				mock.AnythingOfType("[]string"),
-			).Return(parsedData)
+		client.On("UpdateRecord",
+			mock.Anything, // ctx is irrelevant for this test
+			mock.AnythingOfType("*dns.RecordBody"),
+			"exampleterraform.io",
+			mock.Anything,
+		).Return(nil)
 
-			client.On("DeleteRecord",
-				mock.Anything, // ctx is irrelevant for this test
-				mock.AnythingOfType("*dns.RecordBody"),
-				"exampleterraform.io",
-				mock.Anything,
-			).Return(nil)
+		client.On("ParseRData",
+			mock.Anything,
+			"A",
+			mock.AnythingOfType("[]string"),
+		).Return(parsedData)
 
-			dataSourceName := "akamai_dns_record.a_record"
+		client.On("DeleteRecord",
+			mock.Anything, // ctx is irrelevant for this test
+			mock.AnythingOfType("*dns.RecordBody"),
+			"exampleterraform.io",
+			mock.Anything,
+		).Return(nil)
 
-			useClient(client, func() {
-				resource.UnitTest(t, resource.TestCase{
-					PreCheck:  func() { testAccPreCheck(t) },
-					Providers: testAccProviders,
-					Steps: []resource.TestStep{
-						{
-							// use the update config because the rec value was changed in the previous example
-							Config: loadFixtureString("testdata/TestResDnsRecord/update_basic.tf"),
-							Check: resource.ComposeTestCheckFunc(
-								resource.TestCheckResourceAttr(dataSourceName, "zone", "exampleterraform.io"),
-								resource.TestCheckResourceAttr(dataSourceName, "name", "exampleterraform.io"),
-								resource.TestCheckResourceAttr(dataSourceName, "recordtype", "A"),
-							),
-						},
+		dataSourceName := "akamai_dns_record.a_record"
+
+		useClient(client, func() {
+			resource.UnitTest(t, resource.TestCase{
+				PreCheck:  func() { testAccPreCheck(t) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						// use the update config because the rec value was changed in the previous example
+						Config: loadFixtureString("testdata/TestResDnsRecord/update_basic.tf"),
+						Check: resource.ComposeTestCheckFunc(
+							resource.TestCheckResourceAttr(dataSourceName, "zone", "exampleterraform.io"),
+							resource.TestCheckResourceAttr(dataSourceName, "name", "exampleterraform.io"),
+							resource.TestCheckResourceAttr(dataSourceName, "recordtype", "A"),
+						),
 					},
-				})
+				},
 			})
-
-			client.AssertExpectations(t)
 		})
 
-		// This test does an "update" by returning empty rdata which forces a new record overrite
-		// It is not a full lifecycle test as the "update" occurs in the create method
-		t.Run("save record", func(t *testing.T) {
-			client := &mockdns{}
+		client.AssertExpectations(t)
+	})
 
-			client.On("GetRecord",
-				mock.Anything, // ctx is irrelevant for this test
-				"exampleterraform.io",
-				"exampleterraform.io",
-				"A",
-			).Return(rec, nil)
+	// This test does an "update" by returning empty rdata which forces a new record overrite
+	// It is not a full lifecycle test as the "update" occurs in the create method
+	t.Run("save record", func(t *testing.T) {
+		client := &mockdns{}
 
-			// return empty rdata to trigger the "save" codepath
-			client.On("ProcessRdata",
-				mock.Anything, // ctx is irrelevant for this test
-				rec.Target,
-				"A",
-			).Return([]string{}, nil).Once().Run(func(mock.Arguments) {
-				// return valid rdata so save succeeds
-				client.On("ProcessRdata",
-					mock.Anything, // ctx is irrelevant for this test
-					mock.AnythingOfType("[]string"),
-					"A",
-				).Return(rec.Target, nil)
-			})
+		client.On("GetRecord",
+			mock.Anything, // ctx is irrelevant for this test
+			"exampleterraform.io",
+			"exampleterraform.io",
+			"A",
+		).Return(rec, nil)
 
-			client.On("CreateRecord",
-				mock.Anything, // ctx is irrelevant for this test
-				mock.AnythingOfType("*dns.RecordBody"),
-				"exampleterraform.io",
-				mock.Anything,
-			).Return(nil)
+		// return empty rdata to trigger the "save" codepath
+		procCall := client.On("ProcessRdata",
+			mock.Anything, // ctx is irrelevant for this test
+			rec.Target,
+			"A",
+		).Return([]string{}, nil)
 
-			client.On("ParseRData",
-				mock.Anything,
-				"A",
-				mock.AnythingOfType("[]string"),
-			).Return(parsedData)
-
-			client.On("DeleteRecord",
-				mock.Anything, // ctx is irrelevant for this test
-				mock.AnythingOfType("*dns.RecordBody"),
-				"exampleterraform.io",
-				mock.Anything,
-			).Return(nil)
-
-			dataSourceName := "akamai_dns_record.a_record"
-
-			useClient(client, func() {
-				resource.UnitTest(t, resource.TestCase{
-					PreCheck:  func() { testAccPreCheck(t) },
-					Providers: testAccProviders,
-					Steps: []resource.TestStep{
-						{
-							Config: loadFixtureString("testdata/TestResDnsRecord/update_basic.tf"),
-							Check: resource.ComposeTestCheckFunc(
-								resource.TestCheckResourceAttr(dataSourceName, "zone", "exampleterraform.io"),
-								resource.TestCheckResourceAttr(dataSourceName, "name", "exampleterraform.io"),
-								resource.TestCheckResourceAttr(dataSourceName, "recordtype", "A"),
-							),
-						},
-					},
-				})
-			})
-
-			client.AssertExpectations(t)
+		client.On("CreateRecord",
+			mock.Anything, // ctx is irrelevant for this test
+			mock.AnythingOfType("*dns.RecordBody"),
+			"exampleterraform.io",
+			mock.Anything,
+		).Return(nil).Run(func(mock.Arguments) {
+			procCall.ReturnArguments = mock.Arguments{rec.Target, nil}
 		})
 
-		soaRec := &dns.RecordBody{
-			RecordType: "SOA",
-			Name:       "exampleterraform.io",
-			Target:     []string{"ns1.exampleterraform.io root@exampleterraform.io 123456789 3600 600 3600 3600"},
-			TTL:        300,
-		}
+		client.On("ParseRData",
+			mock.Anything,
+			"A",
+			mock.AnythingOfType("[]string"),
+		).Return(parsedData)
 
-		t.Run("create soa record", func(t *testing.T) {
-			client := &mockdns{}
+		client.On("DeleteRecord",
+			mock.Anything, // ctx is irrelevant for this test
+			mock.AnythingOfType("*dns.RecordBody"),
+			"exampleterraform.io",
+			mock.Anything,
+		).Return(nil)
 
-			count := 0
+		dataSourceName := "akamai_dns_record.a_record"
 
-			client.On("GetRecord",
-				mock.Anything, // ctx is irrelevant for this test
-				"exampleterraform.io",
-				"@",
-				"SOA",
-			).Return(nil, &dns.Error{
-				StatusCode: http.StatusNotFound,
-			}).Twice().Run(func(mock.Arguments) {
-				if count < 1 {
-					count++
-					return
-				}
-				client.On("GetRecord",
-					mock.Anything, // ctx is irrelevant for this test
-					"exampleterraform.io",
-					"@",
-					"SOA",
-				).Return(soaRec, nil)
-
-				client.On("ProcessRdata",
-					mock.Anything, // ctx is irrelevant for this test
-					mock.AnythingOfType("[]string"),
-					"SOA",
-				).Return(soaRec.Target, nil)
-			})
-
-			client.On("CreateRecord",
-				mock.Anything, // ctx is irrelevant for this test
-				mock.AnythingOfType("*dns.RecordBody"),
-				"exampleterraform.io",
-				mock.Anything,
-			).Return(nil)
-
-			client.On("ParseRData",
-				mock.Anything,
-				"SOA",
-				mock.AnythingOfType("[]string"),
-			).Return(parseRData(context.Background(), "SOA", soaRec.Target))
-
-			client.On("DeleteRecord",
-				mock.Anything, // ctx is irrelevant for this test
-				mock.AnythingOfType("*dns.RecordBody"),
-				"exampleterraform.io",
-				mock.Anything,
-			).Return(nil)
-
-			dataSourceName := "akamai_dns_record.soa_record"
-
-			useClient(client, func() {
-				resource.UnitTest(t, resource.TestCase{
-					PreCheck:  func() { testAccPreCheck(t) },
-					Providers: testAccProviders,
-					Steps: []resource.TestStep{
-						{
-							Config: loadFixtureString("testdata/TestResDnsRecord/create_soa.tf"),
-							Check: resource.ComposeTestCheckFunc(
-								resource.TestCheckResourceAttr(dataSourceName, "zone", "exampleterraform.io"),
-								resource.TestCheckResourceAttr(dataSourceName, "recordtype", "SOA"),
-							),
-						},
+		useClient(client, func() {
+			resource.UnitTest(t, resource.TestCase{
+				PreCheck:  func() { testAccPreCheck(t) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config: loadFixtureString("testdata/TestResDnsRecord/update_basic.tf"),
+						Check: resource.ComposeTestCheckFunc(
+							resource.TestCheckResourceAttr(dataSourceName, "zone", "exampleterraform.io"),
+							resource.TestCheckResourceAttr(dataSourceName, "name", "exampleterraform.io"),
+							resource.TestCheckResourceAttr(dataSourceName, "recordtype", "A"),
+						),
 					},
-				})
+				},
 			})
-
-			client.AssertExpectations(t)
 		})
 
-		t.Run("update soa record", func(t *testing.T) {
-			client := &mockdns{}
+		client.AssertExpectations(t)
+	})
 
-			client.On("GetRecord",
-				mock.Anything, // ctx is irrelevant for this test
-				"exampleterraform.io",
-				"@",
-				"SOA",
-			).Return(soaRec, nil)
+	soaRec := &dns.RecordBody{
+		RecordType: "SOA",
+		Name:       "exampleterraform.io",
+		Target:     []string{"ns1.exampleterraform.io root@exampleterraform.io 123456789 3600 600 3600 3600"},
+		TTL:        300,
+	}
 
-			client.On("ProcessRdata",
-				mock.Anything, // ctx is irrelevant for this test
-				mock.AnythingOfType("[]string"),
-				"SOA",
-			).Return(soaRec.Target, nil)
+	t.Run("create soa record", func(t *testing.T) {
+		client := &mockdns{}
 
-			client.On("UpdateRecord",
-				mock.Anything, // ctx is irrelevant for this test
-				mock.AnythingOfType("*dns.RecordBody"),
-				"exampleterraform.io",
-				mock.Anything,
-			).Return(nil)
+		getCall := client.On("GetRecord",
+			mock.Anything, // ctx is irrelevant for this test
+			"exampleterraform.io",
+			"@",
+			"SOA",
+		).Return(nil, &dns.Error{
+			StatusCode: http.StatusNotFound,
+		})
 
-			client.On("ParseRData",
-				mock.Anything,
-				"SOA",
-				mock.AnythingOfType("[]string"),
-			).Return(parseRData(context.Background(), "SOA", soaRec.Target))
+		client.On("CreateRecord",
+			mock.Anything, // ctx is irrelevant for this test
+			mock.AnythingOfType("*dns.RecordBody"),
+			"exampleterraform.io",
+			mock.Anything,
+		).Return(nil).Run(func(mock.Arguments) {
+			getCall.ReturnArguments = mock.Arguments{soaRec, nil}
+		})
 
-			client.On("DeleteRecord",
-				mock.Anything, // ctx is irrelevant for this test
-				mock.AnythingOfType("*dns.RecordBody"),
-				"exampleterraform.io",
-				mock.Anything,
-			).Return(nil)
+		client.On("ProcessRdata",
+			mock.Anything, // ctx is irrelevant for this test
+			mock.AnythingOfType("[]string"),
+			"SOA",
+		).Return(soaRec.Target, nil)
 
-			dataSourceName := "akamai_dns_record.soa_record"
+		client.On("ParseRData",
+			mock.Anything,
+			"SOA",
+			mock.AnythingOfType("[]string"),
+		).Return(parseRData(context.Background(), "SOA", soaRec.Target))
 
-			useClient(client, func() {
-				resource.UnitTest(t, resource.TestCase{
-					PreCheck:  func() { testAccPreCheck(t) },
-					Providers: testAccProviders,
-					Steps: []resource.TestStep{
-						{
-							Config: loadFixtureString("testdata/TestResDnsRecord/create_soa.tf"),
-							Check: resource.ComposeTestCheckFunc(
-								resource.TestCheckResourceAttr(dataSourceName, "zone", "exampleterraform.io"),
-								resource.TestCheckResourceAttr(dataSourceName, "recordtype", "SOA"),
-							),
-						},
+		client.On("DeleteRecord",
+			mock.Anything, // ctx is irrelevant for this test
+			mock.AnythingOfType("*dns.RecordBody"),
+			"exampleterraform.io",
+			mock.Anything,
+		).Return(nil)
+
+		dataSourceName := "akamai_dns_record.soa_record"
+
+		useClient(client, func() {
+			resource.UnitTest(t, resource.TestCase{
+				PreCheck:  func() { testAccPreCheck(t) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config: loadFixtureString("testdata/TestResDnsRecord/create_soa.tf"),
+						Check: resource.ComposeTestCheckFunc(
+							resource.TestCheckResourceAttr(dataSourceName, "zone", "exampleterraform.io"),
+							resource.TestCheckResourceAttr(dataSourceName, "recordtype", "SOA"),
+						),
 					},
-				})
+				},
 			})
+		})
 
-			client.AssertExpectations(t)
-		})*/
+		client.AssertExpectations(t)
+	})
+
+	t.Run("update soa record", func(t *testing.T) {
+		client := &mockdns{}
+
+		client.On("GetRecord",
+			mock.Anything, // ctx is irrelevant for this test
+			"exampleterraform.io",
+			"@",
+			"SOA",
+		).Return(soaRec, nil)
+
+		client.On("ProcessRdata",
+			mock.Anything, // ctx is irrelevant for this test
+			mock.AnythingOfType("[]string"),
+			"SOA",
+		).Return(soaRec.Target, nil)
+
+		client.On("UpdateRecord",
+			mock.Anything, // ctx is irrelevant for this test
+			mock.AnythingOfType("*dns.RecordBody"),
+			"exampleterraform.io",
+			mock.Anything,
+		).Return(nil)
+
+		client.On("ParseRData",
+			mock.Anything,
+			"SOA",
+			mock.AnythingOfType("[]string"),
+		).Return(parseRData(context.Background(), "SOA", soaRec.Target))
+
+		client.On("DeleteRecord",
+			mock.Anything, // ctx is irrelevant for this test
+			mock.AnythingOfType("*dns.RecordBody"),
+			"exampleterraform.io",
+			mock.Anything,
+		).Return(nil)
+
+		dataSourceName := "akamai_dns_record.soa_record"
+
+		useClient(client, func() {
+			resource.UnitTest(t, resource.TestCase{
+				PreCheck:  func() { testAccPreCheck(t) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config: loadFixtureString("testdata/TestResDnsRecord/create_soa.tf"),
+						Check: resource.ComposeTestCheckFunc(
+							resource.TestCheckResourceAttr(dataSourceName, "zone", "exampleterraform.io"),
+							resource.TestCheckResourceAttr(dataSourceName, "recordtype", "SOA"),
+						),
+					},
+				},
+			})
+		})
+
+		client.AssertExpectations(t)
+	})
 }
