6a5e2f5badbb37a716af93cef257530a5a753b8e
Usha Cheekati <ucheekat@akamai.com>
2020-11-10 08:25:52 -0500

    TFP-295: Added readcontext  for property rule with updated schema

--- pkg/providers/property/data_akamai_property_rules.go
+++ pkg/providers/property/data_akamai_property_rules.go
@@ -2,103 +2,139 @@ package property
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v2/pkg/papi"
+	"github.com/akamai/terraform-provider-akamai/v2/pkg/akamai"
+	"github.com/akamai/terraform-provider-akamai/v2/pkg/tools"
 )
 
 func dataPropertyRules() *schema.Resource {
 	return &schema.Resource{
 		ReadContext: dataPropRulesOperation,
-		Schema:      propertyRulesSchemaAttrs(),
+		Schema:      dataPropertyRulesSchema,
 	}
 }
 
-func dataPropRulesOperation(_ context.Context, d *schema.ResourceData, _ interface{}) diag.Diagnostics {
+var dataPropertyRulesSchema = map[string]*schema.Schema{
+	"contract_id": {
+		Type:      schema.TypeString,
+		Required:  true,
+		StateFunc: addPrefixToState("ctr_"),
+	},
+	"group_id": {
+		Type:      schema.TypeString,
+		Required:  true,
+		StateFunc: addPrefixToState("grp_"),
+	},
+	"property_id": {
+		Type:      schema.TypeString,
+		Required:  true,
+		StateFunc: addPrefixToState("prp_"),
+	},
+	"rules": {
+		Type:        schema.TypeString,
+		Computed:    true,
+		Description: "JSON Rule representation",
+	},
+	"version": {
+		Type:        schema.TypeInt,
+		Optional:    true,
+		Description: "This is a computed value - provider will always use 'latest' version, providing own version number is not supported",
+	},
+	"errors": {
+		Type:     schema.TypeString,
+		Computed: true,
+	},
+}
+
+func dataPropRulesOperation(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	// Always fail for new resources and changed values
 	if d.Id() == "" || d.IsNewResource() || d.HasChanges("variables", "rules") {
 		return diag.Errorf(`data "akamai_property_rules" is no longer supported - See Akamai Terraform Upgrade Guide`)
 	}
 
-	// No changes and resource already exists, must be from previous version of plugin
+	meta := akamai.Meta(m)
+	client := inst.Client(meta)
+
+	var (
+		contractID, groupID, propertyID string
+		version                         int
+		err                             error
+	)
+
+	if contractID, err = tools.GetStringValue("contract_id", d); err != nil {
+		return diag.FromErr(err)
+	}
+	contractID = tools.AddPrefix(contractID, "ctr_")
+	if err := d.Set("contract_id", contractID); err != nil {
+		return diag.FromErr(fmt.Errorf("%w: %s", tools.ErrValueSet, err.Error()))
+	}
+
+	if groupID, err = tools.GetStringValue("group_id", d); err != nil {
+		return diag.FromErr(err)
+	}
+	groupID = tools.AddPrefix(groupID, "grp_")
+	if err := d.Set("group_id", groupID); err != nil {
+		return diag.FromErr(fmt.Errorf("%w: %s", tools.ErrValueSet, err.Error()))
+	}
+
+	if propertyID, err = tools.GetStringValue("property_id", d); err != nil {
+		return diag.FromErr(err)
+	}
+	propertyID = tools.AddPrefix(propertyID, "prp_")
+	if err := d.Set("property_id", propertyID); err != nil {
+		return diag.FromErr(fmt.Errorf("%w: %s", tools.ErrValueSet, err.Error()))
+	}
+
+	if version, err = tools.GetIntValue("version", d); err != nil {
+		latestVersion, err := client.GetLatestVersion(ctx, papi.GetLatestVersionRequest{
+			PropertyID: propertyID,
+			ContractID: contractID,
+			GroupID:    groupID,
+		})
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		version = latestVersion.Version.PropertyVersion
+		if err := d.Set("version", version); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	res, err := client.GetRuleTree(ctx, papi.GetRuleTreeRequest{
+		PropertyID:      propertyID,
+		PropertyVersion: version,
+		ContractID:      contractID,
+		GroupID:         groupID,
+		ValidateRules:   true,
+	})
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	rulesJSON, err := json.Marshal(res.Rules)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("rules", string(rulesJSON)); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if res.Errors != nil {
+		ruleErrors, err := json.Marshal(res.Errors)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		if err := d.Set("errors", string(ruleErrors)); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
 	return nil
-}
-
-// Returns the property rules schema attributes map
-func propertyRulesSchemaAttrs() map[string]*schema.Schema {
-	nameOptionSchema := func() *schema.Schema {
-		return &schema.Schema{
-			Type:     schema.TypeSet,
-			Optional: true,
-			Elem: &schema.Resource{
-				Schema: map[string]*schema.Schema{
-					"name": {Type: schema.TypeString, Required: true},
-					"option": {
-						Type:     schema.TypeSet,
-						Optional: true,
-						Elem: &schema.Resource{
-							Schema: map[string]*schema.Schema{
-								"key":    {Type: schema.TypeString, Required: true},
-								"values": {Type: schema.TypeSet, Optional: true, Elem: &schema.Schema{Type: schema.TypeString}},
-								"value":  {Type: schema.TypeString, Optional: true},
-							},
-						},
-					},
-				},
-			},
-		}
-	}
-
-	// Build a nested rule schema to the given depth
-	var ruleSchema func(int) *schema.Schema
-	ruleSchema = func(depth int) *schema.Schema {
-		attrs := map[string]*schema.Schema{
-			"name":           {Type: schema.TypeString, Required: true},
-			"comment":        {Type: schema.TypeString, Optional: true},
-			"criteria_match": {Type: schema.TypeString, Optional: true, Default: "all"},
-			"criteria":       nameOptionSchema(),
-			"behavior":       nameOptionSchema(),
-		}
-
-		if depth > 0 {
-			attrs["rule"] = ruleSchema(depth - 1)
-		}
-
-		return &schema.Schema{
-			Type:     schema.TypeSet,
-			Optional: true,
-			Elem:     &schema.Resource{Schema: attrs},
-		}
-	}
-
-	return map[string]*schema.Schema{
-		"variables": {Type: schema.TypeString, Optional: true},
-		"json":      {Type: schema.TypeString, Computed: true, Description: "JSON Rule representation"},
-		"rules": {
-			Type:     schema.TypeSet,
-			Optional: true,
-			Elem: &schema.Resource{
-				Schema: map[string]*schema.Schema{
-					"criteria_match": {Type: schema.TypeString, Optional: true, Default: "all"},
-					"is_secure":      {Type: schema.TypeBool, Optional: true, Default: false},
-					"behavior":       nameOptionSchema(),
-					"rule":           ruleSchema(4), // rules tree can go max 5 levels deep (this one + 4 more)
-					"variable": {
-						Type:     schema.TypeSet,
-						Optional: true,
-						Elem: &schema.Resource{
-							Schema: map[string]*schema.Schema{
-								"name":        {Type: schema.TypeString, Required: true},
-								"description": {Type: schema.TypeString, Optional: true},
-								"hidden":      {Type: schema.TypeBool, Required: true},
-								"sensitive":   {Type: schema.TypeBool, Required: true},
-								"value":       {Type: schema.TypeString, Optional: true},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
 }
