be1ab4397db45278028916e05ff878b15cb149b0
López López, Roberto <rlopezlo@akamai.com>
2020-11-13 17:12:42 +0100

    TFP-389: refactor akamai_properties DS output + fix failing test

--- pkg/providers/property/data_akamai_properties.go
+++ pkg/providers/property/data_akamai_properties.go
@@ -2,7 +2,6 @@ package property
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v2/pkg/papi"
 	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v2/pkg/session"
@@ -25,9 +24,25 @@ func dataSourceAkamaiProperties() *schema.Resource {
 				Required: true,
 			},
 			"properties": {
-				Type:        schema.TypeString,
+				Type:        schema.TypeList,
 				Computed:    true,
 				Description: "List of properties",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"account_id":         {Type: schema.TypeString, Computed: true},
+						"asset_id":           {Type: schema.TypeString, Computed: true},
+						"contract_id":        {Type: schema.TypeString, Computed: true},
+						"group_id":           {Type: schema.TypeString, Computed: true},
+						"latest_version":     {Type: schema.TypeInt, Computed: true},
+						"note":               {Type: schema.TypeString, Computed: true},
+						"product_id":         {Type: schema.TypeString, Computed: true},
+						"production_version": {Type: schema.TypeInt, Computed: true},
+						"property_id":        {Type: schema.TypeString, Computed: true},
+						"property_name":      {Type: schema.TypeString, Computed: true},
+						"rule_format":        {Type: schema.TypeString, Computed: true},
+						"staging_version":    {Type: schema.TypeInt, Computed: true},
+					},
+				},
 			},
 		},
 	}
@@ -53,22 +68,44 @@ func dataAkamaiPropertiesRead(ctx context.Context, d *schema.ResourceData, m int
 	}
 	groupId = tools.AddPrefix(groupId, "grp_")
 	contractId = tools.AddPrefix(contractId, "ctr_")
-	properties, err := getProperties(ctx, groupId, contractId, meta)
+	propertiesResponse, err := getProperties(ctx, groupId, contractId, meta)
 	if err != nil {
 		return diag.FromErr(fmt.Errorf("error listing properties: %w", err))
 	}
 	// setting concatenated id to uniquely identify data
 	d.SetId(groupId + contractId)
-	props, err := json.Marshal(properties)
-	if err != nil {
-		return diag.FromErr(err)
+
+	properties := make([]map[string]interface{}, 0)
+	for _, item := range propertiesResponse.Properties.Items {
+		property := map[string]interface{}{
+			"account_id":         item.AccountID,
+			"asset_id":           item.AssetID,
+			"contract_id":        item.ContractID,
+			"group_id":           item.GroupID,
+			"latest_version":     item.LatestVersion,
+			"note":               item.Note,
+			"product_id":         item.ProductID,
+			"production_version": decodeVersion(item.ProductionVersion),
+			"property_id":        item.PropertyID,
+			"property_name":      item.PropertyName,
+			"rule_format":        item.RuleFormat,
+			"staging_version":    decodeVersion(item.StagingVersion),
+		}
+		properties = append(properties, property)
 	}
-	/* setting raw json here in current scope. We have to set all the json fields in
-	property struct for more granular access to properties object */
-	if err := d.Set("properties", string(props)); err != nil {
+
+	if err := d.Set("properties", properties); err != nil {
 		return diag.FromErr(fmt.Errorf("error setting properties: %s", err))
 	}
 	return nil
+}
+
+func decodeVersion(version interface{}) int {
+	v, ok := version.(*int)
+	if !ok || v == nil {
+		return 0
+	}
+	return *v
 }
 
 // Reusable function to fetch all the properties for a given group and contract
--- pkg/providers/property/testdata/TestDSPropertyRules/always_fails.tf
+++ pkg/providers/property/testdata/TestDSPropertyRules/always_fails.tf
@@ -2,4 +2,8 @@ provider "akamai" {
   edgerc = "~/.edgerc"
 }
 
-data "akamai_property_rules" "rules" {}
+data "akamai_property_rules" "rules" {
+  contract_id = "ctr_test"
+  group_id = "grp_test"
+  property_id = "prp_test"
+}
