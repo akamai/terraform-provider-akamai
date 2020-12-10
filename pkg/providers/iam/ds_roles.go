eecead8c54a2a30ea3d8c3dafef5c33fd6cfbbac
Erik Elmore <erik@elmore.io>
2020-12-10 13:00:31 -0800

    Add IAM roles data source

--- pkg/providers/iam/ds_roles.go
+++ pkg/providers/iam/ds_roles.go
@@ -2,7 +2,10 @@ package iam
 
 import (
 	"context"
+	"strconv"
 
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v2/pkg/iam"
+	"github.com/apex/log"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -14,9 +17,9 @@ func (p *provider) dsRoles() *schema.Resource {
 		Schema: map[string]*schema.Schema{
 			// inputs
 			"group_id": {
-				Type:        schema.TypeInt,
+				Type:        schema.TypeString,
 				Description: "A unique identifier for a group",
-				Required:    true,
+				Optional:    true,
 			},
 			"get_actions": {
 				Type:        schema.TypeBool,
@@ -28,109 +31,127 @@ func (p *provider) dsRoles() *schema.Resource {
 				Description: "When enabled, returns users assigned to the roles",
 				Optional:    true,
 			},
-			"ignore_client_context": {
+			"ignore_context": {
 				Type:        schema.TypeBool,
 				Description: "When enabled, returns all roles for the current account without regard the contract type associated with your API client",
 				Optional:    true,
 			},
 
 			// outputs
-			"role_id": {
-				Type:        schema.TypeInt,
-				Description: "A unique identifier for each role",
-				Computed:    true,
-			},
-			"name": {
-				Type:        schema.TypeString,
-				Description: "The role's name",
-				Computed:    true,
-			},
-			"description": {
-				Type:        schema.TypeString,
-				Description: "The role's description",
-				Computed:    true,
-			},
-			"type": {
-				Type:        schema.TypeString,
-				Description: "Whether the role is a standard role or a custom role",
-				Computed:    true,
-			},
-			"time_created": {
-				Type:        schema.TypeString,
-				Description: "ISO 8601 timestamp indicating when the role was originally created",
-				Computed:    true,
-			},
-			"time_modified": {
-				Type:        schema.TypeString,
-				Description: "ISO 8601 timestamp indicating when the role was last updated",
-				Computed:    true,
-			},
-			"modified_by": {
-				Type:        schema.TypeString,
-				Description: "The username or email of the last person to edit the role",
-				Computed:    true,
-			},
-			"created_by": {
-				Type:        schema.TypeString,
-				Description: "The user name or email of the person who created the role",
-				Computed:    true,
-			},
-			"user_count": {
-				Type:        schema.TypeInt,
-				Description: "The number of users who use the role",
-				Computed:    true,
-			},
-			"users": {
+			"roles": {
 				Type:        schema.TypeSet,
-				Description: "Permissions available to the user for this group", // These descriptions were taken from the API docs
+				Description: "TODO", // These descriptions were taken from the API docs
 				Computed:    true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"user_id": {
+						"role_id": {
 							Type:        schema.TypeString,
-							Description: "A unique identifier for a user's profile",
+							Description: "A unique identifier for each role",
 							Computed:    true,
 						},
-						"first_name": {
+						"name": {
 							Type:        schema.TypeString,
-							Description: "The user's first name",
+							Description: "The role's name",
 							Computed:    true,
 						},
-						"last_name": {
+						"description": {
 							Type:        schema.TypeString,
-							Description: "The user's last name",
+							Description: "The role's description",
 							Computed:    true,
 						},
-						"account_id": {
+						"type": {
 							Type:        schema.TypeString,
-							Description: "A unique identifier for an account",
+							Description: "Whether the role is a standard role or a custom role",
 							Computed:    true,
 						},
-						"email": {
+						"time_created": {
 							Type:        schema.TypeString,
-							Description: "The user's email address",
+							Description: "ISO 8601 timestamp indicating when the role was originally created",
 							Computed:    true,
 						},
-						"last_login": {
+						"time_modified": {
 							Type:        schema.TypeString,
-							Description: "ISO 8601 timestamp indicating when the user last logged in",
+							Description: "ISO 8601 timestamp indicating when the role was last updated",
 							Computed:    true,
 						},
-					},
-				},
-			},
-			"actions": {
-				Type:        schema.TypeSet,
-				Description: "Permissions available to the user for this group",
-				Computed:    true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"delete": {
+						"modified_by": {
+							Type:        schema.TypeString,
+							Description: "The username or email of the last person to edit the role",
+							Computed:    true,
+						},
+						"created_by": {
+							Type:        schema.TypeString,
+							Description: "The user name or email of the person who created the role",
+							Computed:    true,
+						},
+						"users": {
+							Type:        schema.TypeSet,
+							Description: "Permissions available to the user for this group", // These descriptions were taken from the API docs
+							Computed:    true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"user_id": {
+										Type:        schema.TypeString,
+										Description: "A unique identifier for a user's profile",
+										Computed:    true,
+									},
+									"first_name": {
+										Type:        schema.TypeString,
+										Description: "The user's first name",
+										Computed:    true,
+									},
+									"last_name": {
+										Type:        schema.TypeString,
+										Description: "The user's last name",
+										Computed:    true,
+									},
+									"account_id": {
+										Type:        schema.TypeString,
+										Description: "A unique identifier for an account",
+										Computed:    true,
+									},
+									"email": {
+										Type:        schema.TypeString,
+										Description: "The user's email address",
+										Computed:    true,
+									},
+									"last_login": {
+										Type:        schema.TypeString,
+										Description: "ISO 8601 timestamp indicating when the user last logged in",
+										Computed:    true,
+									},
+								},
+							},
+						},
+						"granted_roles": {
+							Type:     schema.TypeSet,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"role_id": {
+										Type:        schema.TypeString,
+										Description: "TODO",
+										Computed:    true,
+									},
+									"name": {
+										Type:        schema.TypeString,
+										Description: "TODO",
+										Computed:    true,
+									},
+									"description": {
+										Type:        schema.TypeString,
+										Description: "TODO",
+										Computed:    true,
+									},
+								},
+							},
+						},
+						"delete_allowed": {
 							Type:        schema.TypeBool,
 							Description: "Indicates whether the user can remove items from the group",
 							Computed:    true,
 						},
-						"edit": {
+						"edit_allowed": {
 							Type:        schema.TypeBool,
 							Description: "Indicates whether the user can modify items in the group",
 							Computed:    true,
@@ -143,5 +164,111 @@ func (p *provider) dsRoles() *schema.Resource {
 }
 
 func (p *provider) dsRolesRead(ctx context.Context, d *schema.ResourceData, _ interface{}) diag.Diagnostics {
+	logger := log.FromContext(ctx)
+
+	getActions := d.Get("get_actions").(bool)
+	getUsers := d.Get("get_users").(bool)
+	IgnoreContext := d.Get("ignore_context").(bool)
+
+	GroupID, err := strconv.ParseInt(d.Get("group_id").(string), 10, 64)
+	if err != nil {
+		logger.WithError(err).Error("group_id must be an integer")
+		return diag.FromErr(err)
+	}
+
+	logger.Debug("Fetching roles")
+	req := iam.ListRolesRequest{
+		GroupID:       &GroupID,
+		Actions:       getActions,
+		IgnoreContext: IgnoreContext,
+		Users:         getUsers,
+	}
+	res, err := p.client.ListRoles(ctx, req)
+	if err != nil {
+		logger.WithError(err).Error("Could not get roles")
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("roles", rolesToState(res)); err != nil {
+		logger.WithError(err).Error("Could not set roles in state")
+		return diag.FromErr(err)
+	}
+
+	d.SetId("akamai_iam_roles")
 	return nil
+}
+
+func rolesToState(roles []iam.Role) []interface{} {
+	var out []interface{}
+
+	for _, r := range roles {
+		out = append(out, roleToState(r))
+	}
+
+	return out
+}
+
+func roleToState(r iam.Role) map[string]interface{} {
+	m := map[string]interface{}{}
+
+	m["role_id"] = strconv.FormatInt(r.RoleID, 10)
+	m["name"] = r.RoleName
+	m["description"] = r.RoleDescription
+	m["type"] = string(r.RoleType)
+	m["time_created"] = r.CreatedDate
+	m["time_modified"] = r.ModifiedDate
+	m["modified_by"] = r.ModifiedBy
+	m["created_by"] = r.CreatedBy
+	m["users"] = usersToState(r.Users)
+	m["granted_roles"] = grantedRolesToState(r.GrantedRoles)
+
+	if r.Actions != nil {
+		m["edit_allowed"] = r.Actions.Edit
+		m["delete_allowed"] = r.Actions.Delete
+	}
+
+	return m
+}
+
+func grantedRolesToState(roles []iam.RoleGrantedRole) []interface{} {
+	var out []interface{}
+
+	for _, r := range roles {
+		out = append(out, grantedRoleToState(r))
+	}
+
+	return out
+}
+
+func grantedRoleToState(r iam.RoleGrantedRole) map[string]interface{} {
+	m := map[string]interface{}{}
+
+	m["name"] = r.RoleName
+	m["role_id"] = strconv.FormatInt(r.RoleID, 10)
+	m["description"] = r.Description
+
+	return m
+}
+
+func usersToState(users []iam.RoleUser) []interface{} {
+	var out []interface{}
+
+	for _, r := range users {
+		out = append(out, userToState(r))
+	}
+
+	return out
+}
+
+func userToState(u iam.RoleUser) map[string]interface{} {
+	m := map[string]interface{}{}
+
+	m["user_id"] = u.UIIdentityID
+	m["first_name"] = u.FirstName
+	m["last_name"] = u.LastName
+	m["account_id"] = u.AccountID
+	m["email"] = u.Email
+	m["last_login"] = u.LastLoginDate
+
+	return m
 }
--- pkg/providers/iam/ds_roles_test.go
+++ pkg/providers/iam/ds_roles_test.go
@@ -3,9 +3,99 @@ package iam
 import (
 	"testing"
 
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v2/pkg/iam"
 	"github.com/akamai/terraform-provider-akamai/v2/pkg/test"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/stretchr/testify/mock"
 )
 
-func TestDSRole(t *testing.T) {
-	test.TODO(t, "Not implemented")
+func TestDSRoles(t *testing.T) {
+	t.Parallel()
+
+	t.Run("happy path", func(t *testing.T) {
+		t.Parallel()
+
+		roles := []iam.Role{{
+			RoleName:        "test role name",
+			RoleID:          100,
+			RoleDescription: "role description",
+			RoleType:        iam.RoleTypeStandard,
+			CreatedBy:       "creator@akamai.net",
+			CreatedDate:     "2020-01-01T00:00:00Z",
+			ModifiedBy:      "modifier@akamai.net",
+			ModifiedDate:    "2020-01-01T00:00:00Z",
+			GrantedRoles: []iam.RoleGrantedRole{{
+				RoleName:    "granted test role name",
+				RoleID:      101,
+				Description: "granted role description",
+			}},
+			Users: []iam.RoleUser{{
+				UIIdentityID:  "300",
+				Email:         "user@akamai.net",
+				FirstName:     "user's first name",
+				LastName:      "user's last name",
+				AccountID:     "200",
+				LastLoginDate: "2020-01-01T00:00:00Z",
+			}},
+			Actions: &iam.RoleAction{
+				Delete: true,
+				Edit:   true,
+			},
+		}}
+
+		gid := int64(300)
+		req := iam.ListRolesRequest{
+			GroupID:       &gid,
+			Actions:       true,
+			IgnoreContext: true,
+			Users:         true,
+		}
+
+		client := &IAM{}
+		client.Test(test.TattleT{T: t})
+		client.On("ListRoles", mock.Anything, req).Return(roles, nil)
+
+		p := provider{}
+		p.SetCache(metaCache{})
+		p.SetClient(client)
+
+		resource.UnitTest(t, resource.TestCase{
+			ProviderFactories: p.ProviderFactories(),
+			Steps: []resource.TestStep{
+				{
+					Config: test.Fixture("testdata/%s/step0.tf", t.Name()),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttrSet("data.akamai_iam_roles.test", "id"),
+						resource.TestCheckTypeSetElemNestedAttrs("data.akamai_iam_roles.test", "roles.*", map[string]string{
+							"name":           "test role name",
+							"role_id":        "100",
+							"description":    "role description",
+							"type":           string(iam.RoleTypeStandard),
+							"time_created":   "2020-01-01T00:00:00Z",
+							"time_modified":  "2020-01-01T00:00:00Z",
+							"created_by":     "creator@akamai.net",
+							"modified_by":    "modifier@akamai.net",
+							"edit_allowed":   "true",
+							"delete_allowed": "true",
+						}),
+						resource.TestCheckTypeSetElemNestedAttrs("data.akamai_iam_roles.test", "roles.*.granted_roles.*", map[string]string{
+							"name":        "granted test role name",
+							"role_id":     "101",
+							"description": "granted role description",
+						}),
+						resource.TestCheckTypeSetElemNestedAttrs("data.akamai_iam_roles.test", "roles.*.users.*", map[string]string{
+							"user_id":    "300",
+							"email":      "user@akamai.net",
+							"first_name": "user's first name",
+							"last_name":  "user's last name",
+							"account_id": "200",
+							"last_login": "2020-01-01T00:00:00Z",
+						}),
+					),
+				},
+			},
+		})
+
+		client.AssertExpectations(t)
+	})
 }
