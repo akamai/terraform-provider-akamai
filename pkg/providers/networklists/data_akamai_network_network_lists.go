9c738bd6341ec96e8446051a09c6e06e6c5cdb10
Philip Lodine <plodine@akamai.com>
2021-02-26 12:32:33 -0500

    Support type and list parameters

--- pkg/providers/networklists/data_akamai_network_network_lists.go
+++ pkg/providers/networklists/data_akamai_network_network_lists.go
@@ -11,6 +11,7 @@ import (
 	"github.com/akamai/terraform-provider-akamai/v2/pkg/tools"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
 
 func dataSourceNetworkList() *schema.Resource {
@@ -18,13 +19,23 @@ func dataSourceNetworkList() *schema.Resource {
 		ReadContext: dataSourceNetworkListRead,
 		Schema: map[string]*schema.Schema{
 			"name": {
-				Type:     schema.TypeString,
-				Optional: true,
+				Type:         schema.TypeString,
+				Optional:     true,
+				RequiredWith: []string{"type"},
 			},
 			"uniqueid": {
 				Type:        schema.TypeString,
 				Computed:    true,
 				Description: "uniqueId",
+			},
+			"type": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					IP,
+					Geo,
+				}, false),
+				RequiredWith: []string{"name"},
 			},
 			"json": {
 				Type:     schema.TypeString,
@@ -57,6 +68,11 @@ func dataSourceNetworkListRead(ctx context.Context, d *schema.ResourceData, m in
 	}
 	getNetworkList.Name = name
 
+	_type, err := tools.GetStringValue("type", d)
+	if err != nil && !errors.Is(err, tools.ErrNotFound) {
+		return diag.FromErr(err)
+	}
+
 	networklist, err := client.GetNetworkLists(ctx, getNetworkList)
 	if err != nil {
 		logger.Errorf("calling 'getNetworkLists': %s", err.Error())
@@ -64,27 +80,53 @@ func dataSourceNetworkListRead(ctx context.Context, d *schema.ResourceData, m in
 	}
 
 	if len(networklist.NetworkLists) > 0 {
-		d.SetId(networklist.NetworkLists[0].UniqueID)
-		if err := d.Set("uniqueid", networklist.NetworkLists[0].UniqueID); err != nil {
-			return diag.FromErr(fmt.Errorf("%w: %s", tools.ErrValueSet, err.Error()))
+		if len(name) > 0 {
+			for _, networkList := range networklist.NetworkLists {
+				if networkList.UniqueID == name && networkList.Type == _type {
+					d.SetId(networkList.UniqueID)
+					if err := d.Set("uniqueid", networkList.UniqueID); err != nil {
+						return diag.FromErr(fmt.Errorf("%w: %s", tools.ErrValueSet, err.Error()))
+					}
+
+					jsonBody, err := json.Marshal(networkList)
+					if err != nil {
+						return diag.FromErr(err)
+					}
+					if err := d.Set("json", string(jsonBody)); err != nil {
+						return diag.FromErr(fmt.Errorf("%w: %s", tools.ErrValueSet, err.Error()))
+					}
+
+					ids := make([]string, 0, 1)
+					ids = append(ids, networkList.UniqueID)
+					if err := d.Set("list", ids); err != nil {
+						logger.Errorf("error setting 'list': %s", err.Error())
+						return diag.FromErr(fmt.Errorf("%w: %s", tools.ErrValueSet, err.Error()))
+					}
+					break // can there be more than one list with same name & type?
+				}
+			}
+		} else {
+			d.SetId(networklist.NetworkLists[0].UniqueID)
+			if err := d.Set("uniqueid", networklist.NetworkLists[0].UniqueID); err != nil {
+				return diag.FromErr(fmt.Errorf("%w: %s", tools.ErrValueSet, err.Error()))
+			}
+			jsonBody, err := json.Marshal(networklist)
+			if err != nil {
+				return diag.FromErr(err)
+			}
+			if err := d.Set("json", string(jsonBody)); err != nil {
+				return diag.FromErr(fmt.Errorf("%w: %s", tools.ErrValueSet, err.Error()))
+			}
+
+			ids := make([]string, 0, len(networklist.NetworkLists))
+			for _, networkList := range networklist.NetworkLists {
+				ids = append(ids, networkList.UniqueID)
+			}
+			if err := d.Set("list", ids); err != nil {
+				logger.Errorf("error setting 'list': %s", err.Error())
+				return diag.FromErr(fmt.Errorf("%w: %s", tools.ErrValueSet, err.Error()))
+			}
 		}
-	}
-
-	jsonBody, err := json.Marshal(networklist)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	if err := d.Set("json", string(jsonBody)); err != nil {
-		return diag.FromErr(fmt.Errorf("%w: %s", tools.ErrValueSet, err.Error()))
-	}
-
-	ids := make([]string, 0, len(networklist.NetworkLists))
-	for _, networkList := range networklist.NetworkLists {
-		ids = append(ids, networkList.UniqueID)
-	}
-	if err := d.Set("list", ids); err != nil {
-		return diag.FromErr(fmt.Errorf("%w: %s", tools.ErrValueSet, err.Error()))
 	}
 
 	return nil
