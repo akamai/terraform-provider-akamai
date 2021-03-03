46f1e6cd2456738e89107011f18fe71b4623686f
martinstibbe <33664051+martinstibbe@users.noreply.github.com>
2021-03-03 15:54:12 -0600

    Fix Networklist description testing

--- pkg/providers/networklists/resource_akamai_networklist_network_list_subscription_test.go
+++ pkg/providers/networklists/resource_akamai_networklist_network_list_subscription_test.go
@@ -21,15 +21,24 @@ func TestAccAkamaiNetworkListSubscription_res_basic(t *testing.T) {
 		expectJS := compactJSON(loadFixtureBytes("testdata/TestResNetworkListSubscription/NetworkListSubscription.json"))
 		json.Unmarshal([]byte(expectJS), &cr)
 
+		cd := networklists.RemoveNetworkListSubscriptionResponse{}
+		expectJSD := compactJSON(loadFixtureBytes("testdata/TestResNetworkListSubscription/NetworkListSubscription.json"))
+		json.Unmarshal([]byte(expectJSD), &cd)
+
 		client.On("GetNetworkListSubscription",
 			mock.Anything, // ctx is irrelevant for this test
-			networklists.GetNetworkListSubscriptionRequest{}, //Recipients: "79536_MARTINNETWORKLIST", UniqueIds: "test@email.com"},
+			networklists.GetNetworkListSubscriptionRequest{Recipients: []string{"test@email.com"}, UniqueIds: []string{"79536_MARTINNETWORKLIST"}}, //Recipients: "79536_MARTINNETWORKLIST", UniqueIds: "test@email.com"},
 		).Return(&cr, nil)
 
 		client.On("UpdateNetworkListSubscription",
 			mock.Anything, // ctx is irrelevant for this test
-			networklists.UpdateNetworkListSubscriptionRequest{}, //Recipients: ,
+			networklists.UpdateNetworkListSubscriptionRequest{Recipients: []string{"test@email.com"}, UniqueIds: []string{"79536_MARTINNETWORKLIST"}}, //Recipients: ,
 		).Return(&cu, nil)
+
+		client.On("RemoveNetworkListSubscription",
+			mock.Anything, // ctx is irrelevant for this test
+			networklists.RemoveNetworkListSubscriptionRequest{Recipients: []string{"test@email.com"}, UniqueIds: []string{"79536_MARTINNETWORKLIST"}}, //Recipients: ,
+		).Return(&cd, nil)
 
 		useClient(client, func() {
 			resource.Test(t, resource.TestCase{
