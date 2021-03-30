9dc0da9f22c14ecbaa1a80ae8d2d1db4b4d4b95f
martinstibbe <33664051+martinstibbe@users.noreply.github.com>
2021-03-30 17:20:51 -0500

    set update to resourceConfigurationVersionCloneCreate

--- pkg/providers/appsec/resource_akamai_appsec_configuration_version_clone.go
+++ pkg/providers/appsec/resource_akamai_appsec_configuration_version_clone.go
@@ -20,7 +20,7 @@ func resourceConfigurationVersionClone() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: resourceConfigurationVersionCloneCreate,
 		ReadContext:   resourceConfigurationVersionCloneRead,
-		UpdateContext: resourceConfigurationVersionCloneUpdate,
+		UpdateContext: resourceConfigurationVersionCloneCreate,
 		DeleteContext: resourceConfigurationVersionCloneDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
