cdc7afd843e5fb40a168ce51b890476ea1b80ece
Piotrowski, Piotr <pipiotro@akamai.com>
2021-04-26 14:17:30 +0200

    [TFP-697] Add core CPS subprovider

---
+++ pkg/providers/cps/provider.go
@@ -0,0 +1,94 @@
+package cps
+
+import (
+	"sync"
+
+	"github.com/apex/log"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v2/pkg/cps"
+	"github.com/akamai/terraform-provider-akamai/v2/pkg/akamai"
+)
+
+type (
+	provider struct {
+		*schema.Provider
+
+		client cps.CPS
+	}
+
+	// Option is a cps provider option
+	Option func(p *provider)
+)
+
+var (
+	once sync.Once
+
+	inst *provider
+)
+
+// Subprovider returns a core sub provider
+func Subprovider(opts ...Option) akamai.Subprovider {
+	once.Do(func() {
+		inst = &provider{Provider: Provider()}
+
+		for _, opt := range opts {
+			opt(inst)
+		}
+	})
+
+	return inst
+}
+
+// Provider returns the Akamai terraform.Resource provider.
+func Provider() *schema.Provider {
+	provider := &schema.Provider{
+		Schema:         map[string]*schema.Schema{},
+		DataSourcesMap: map[string]*schema.Resource{},
+		ResourcesMap:   map[string]*schema.Resource{},
+	}
+	return provider
+}
+
+// WithClient sets the client interface function, used for mocking and testing
+func WithClient(c cps.CPS) Option {
+	return func(p *provider) {
+		p.client = c
+	}
+}
+
+// Client returns the CPS interface
+func (p *provider) Client(meta akamai.OperationMeta) cps.CPS {
+	if p.client != nil {
+		return p.client
+	}
+	return cps.Client(meta.Session())
+}
+
+func (p *provider) Name() string {
+	return "cps"
+}
+
+// ProviderVersion update version string anytime provider adds new features
+const ProviderVersion string = "v0.0.1"
+
+func (p *provider) Version() string {
+	return ProviderVersion
+}
+
+func (p *provider) Schema() map[string]*schema.Schema {
+	return p.Provider.Schema
+}
+
+func (p *provider) Resources() map[string]*schema.Resource {
+	return p.Provider.ResourcesMap
+}
+
+func (p *provider) DataSources() map[string]*schema.Resource {
+	return p.Provider.DataSourcesMap
+}
+
+func (p *provider) Configure(log log.Interface, d *schema.ResourceData) diag.Diagnostics {
+	return nil
+}
