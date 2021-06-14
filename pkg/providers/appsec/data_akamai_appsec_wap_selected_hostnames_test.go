d7d1479368d42320d72ace785e4a5bbb0bddb68c
Philip Lodine <plodine@akamai.com>
2021-06-14 11:33:31 -0400

    SECKSD-10973: handle non-WAP accounts

--- pkg/providers/appsec/data_akamai_appsec_wap_selected_hostnames_test.go
+++ pkg/providers/appsec/data_akamai_appsec_wap_selected_hostnames_test.go
@@ -14,7 +14,7 @@ func TestAccAkamaiWAPSelectedHostnames_data_basic(t *testing.T) {
 		client := &mockappsec{}
 
 		config := appsec.GetConfigurationResponse{}
-		expectConfigs := compactJSON(loadFixtureBytes("testdata/TestResConfiguration/LatestConfiguration.json"))
+		expectConfigs := compactJSON(loadFixtureBytes("testdata/TestResConfiguration/LatestConfigurationWAP.json"))
 		json.Unmarshal([]byte(expectConfigs), &config)
 
 		client.On("GetConfiguration",
@@ -30,6 +30,57 @@ func TestAccAkamaiWAPSelectedHostnames_data_basic(t *testing.T) {
 			mock.Anything,
 			appsec.GetWAPSelectedHostnamesRequest{ConfigID: 43253, Version: 7, SecurityPolicyID: "AAAA_81230"},
 		).Return(&cv, nil)
+
+		useClient(client, func() {
+			resource.Test(t, resource.TestCase{
+				IsUnitTest: true,
+				Providers:  testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config: loadFixtureString("testdata/TestDSWAPSelectedHostnames/match_by_id.tf"),
+						Check: resource.ComposeAggregateTestCheckFunc(
+							resource.TestCheckResourceAttr("data.akamai_appsec_wap_selected_hostnames.test", "id", "43253:AAAA_81230"),
+						),
+					},
+				},
+			})
+		})
+
+		client.AssertExpectations(t)
+	})
+
+}
+
+func TestAccAkamaiWAPSelectedHostnames_NonWAP_data_basic(t *testing.T) {
+	t.Run("match by WAPSelectedHostnames ID", func(t *testing.T) {
+		client := &mockappsec{}
+
+		config := appsec.GetConfigurationResponse{}
+		expectConfigs := compactJSON(loadFixtureBytes("testdata/TestResConfiguration/LatestConfiguration.json"))
+		json.Unmarshal([]byte(expectConfigs), &config)
+
+		client.On("GetConfiguration",
+			mock.Anything,
+			appsec.GetConfigurationRequest{ConfigID: 43253},
+		).Return(&config, nil)
+
+		getSelectedHostnamesResponse := appsec.GetSelectedHostnamesResponse{}
+		expectedGetSelectedHostnamesResponseJSON := compactJSON(loadFixtureBytes("testdata/TestDSSelectedHostnames/SelectedHostnames.json"))
+		json.Unmarshal([]byte(expectedGetSelectedHostnamesResponseJSON), &getSelectedHostnamesResponse)
+
+		getMatchTargetsResponse := appsec.GetMatchTargetsResponse{}
+		expectedGetMatchTargetsResponseJSON := compactJSON(loadFixtureBytes("testdata/TestDSMatchTargets/MatchTargets.json"))
+		json.Unmarshal([]byte(expectedGetMatchTargetsResponseJSON), &getMatchTargetsResponse)
+
+		client.On("GetSelectedHostnames",
+			mock.Anything, // ctx is irrelevant for this test
+			appsec.GetSelectedHostnamesRequest{ConfigID: 43253, Version: 7},
+		).Return(&getSelectedHostnamesResponse, nil)
+
+		client.On("GetMatchTargets",
+			mock.Anything, // ctx is irrelevant for this test
+			appsec.GetMatchTargetsRequest{ConfigID: 43253, ConfigVersion: 7},
+		).Return(&getMatchTargetsResponse, nil)
 
 		useClient(client, func() {
 			resource.Test(t, resource.TestCase{
