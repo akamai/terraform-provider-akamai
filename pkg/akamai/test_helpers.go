d2cf3d861a1f2f3688b019388ced6487ef5312a1
Piotr Piotrowski <pipiotro@akamai.com>
2021-08-10 13:35:28 +0200

    TFP-847 Fix linting issues in test_helpers.go

--- pkg/akamai/test_helpers.go
+++ pkg/akamai/test_helpers.go
@@ -5,8 +5,10 @@ import (
 	"os"
 )
 
+// TFTestTempDir specifies the location of tmp directory which will be used by provider SDK's testing framework
 const TFTestTempDir = "./test_tmp"
 
+// TFTestSetup contains common setup for tests in all subproviders
 func TFTestSetup() error {
 	if err := os.MkdirAll(TFTestTempDir, 0755); err != nil {
 		return fmt.Errorf("test setup failed: %s", err)
@@ -17,6 +19,7 @@ func TFTestSetup() error {
 	return nil
 }
 
+// TFTestTeardown contains common teardown for tests in all subproviders
 func TFTestTeardown() error {
 	if err := os.RemoveAll(TFTestTempDir); err != nil {
 		return fmt.Errorf("test teardown failed: %s", err)
