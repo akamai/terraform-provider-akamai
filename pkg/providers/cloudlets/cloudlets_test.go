fc8ffe442a94a40d8e7fc0eb3de3da6c7f2f5b7b
Piotr Piotrowski <pipiotro@akamai.com>
2021-08-26 15:07:34 +0200

    TFP-909 Cloudlets subprovider initial structure

---
+++ pkg/providers/cloudlets/cloudlets_test.go
@@ -0,0 +1,9 @@
+package cloudlets
+
+import (
+	"github.com/stretchr/testify/mock"
+)
+
+type mockcloudlets struct {
+	mock.Mock
+}
---
+++ pkg/providers/cloudlets/provider_test.go
@@ -0,0 +1,71 @@
+package cloudlets
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"sync"
+	"testing"
+
+	"github.com/akamai/terraform-provider-akamai/v2/pkg/akamai"
+
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v2/pkg/cloudlets"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+var testAccProviders map[string]*schema.Provider
+
+var testProvider *schema.Provider
+
+func TestMain(m *testing.M) {
+	testProvider = akamai.Provider(Subprovider())()
+	testAccProviders = map[string]*schema.Provider{
+		"akamai": testProvider,
+	}
+	if err := akamai.TFTestSetup(); err != nil {
+		log.Fatal(err)
+	}
+	exitCode := m.Run()
+	if err := akamai.TFTestTeardown(); err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(exitCode)
+}
+
+func TestProvider(t *testing.T) {
+	if err := Provider().InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
+
+// Only allow one test at a time to patch the client via useClient()
+var clientLock sync.Mutex
+
+// useClient swaps out the client on the global instance for the duration of the given func
+func useClient(client cloudlets.Cloudlets, f func()) {
+	clientLock.Lock()
+	orig := inst.client
+	inst.client = client
+
+	defer func() {
+		inst.client = orig
+		clientLock.Unlock()
+	}()
+
+	f()
+}
+
+// loadFixtureBytes returns the entire contents of the given file as a byte slice
+func loadFixtureBytes(path string) []byte {
+	contents, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	return contents
+}
+
+// loadFixtureString returns the entire contents of the given file as a string
+func loadFixtureString(format string, args ...interface{}) string {
+	return string(loadFixtureBytes(fmt.Sprintf(format, args...)))
+}
