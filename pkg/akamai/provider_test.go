f91f5ffbc1c2a0bb0c0316ad57d1985791394fdc
Tatiana Slonimskaia <tslonims@akamai.com>
2021-09-03 12:33:12 +0000

    Pull request #415: TFP-872 Refactor: extacted configuration for context as a method. Added unit tests.
    Merge in DEVEXP/terraform-provider-akamai from feature/TFP-872-create-unit-tests-for-provider.go to develop
    
    Squashed commit of the following:
    
    commit 245e3e0036d0961b2199aecc593e6fe39ac827a7
    Author: Tatiana Slonimskaia <tslonims@akamai.com>
    Date:   Fri Sep 3 13:53:30 2021 +0200
    
        TFP-872 Refactor: reordered import.
    
    commit 9c520706f1d1225f98a992a582d302fcd4dac5ee
    Author: Tatiana Slonimskaia <tslonims@akamai.com>
    Date:   Fri Sep 3 12:29:36 2021 +0200
    
        TFP-872 Refactor: renamed variables.
    
    commit 8c624efce245419c5bf72fc4f09019d4dcdfc03a
    Author: Tatiana Slonimskaia <tslonims@akamai.com>
    Date:   Fri Sep 3 12:01:10 2021 +0200
    
        TFP-872 Refactor: renamed variables, changed tests order.
    
    commit 66035f542395f182576274db1e7c0d28e0616465
    Author: Tatiana Slonimskaia <tslonims@akamai.com>
    Date:   Wed Aug 18 14:06:47 2021 +0200
    
        TFP-872 Refactor: extacted configuration for context as a method. Added unit tests.

--- pkg/akamai/provider_test.go
+++ pkg/akamai/provider_test.go
@@ -1,10 +1,17 @@
 package akamai
 
 import (
-	"github.com/stretchr/testify/require"
-	"github.com/tj/assert"
+	"context"
+	"errors"
 	"os"
 	"testing"
+
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v2/pkg/edgegrid"
+	"github.com/akamai/terraform-provider-akamai/v2/pkg/tools"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/stretchr/testify/require"
+	"github.com/tj/assert"
 )
 
 func TestSetEdgegridEnvs(t *testing.T) {
@@ -97,5 +104,302 @@ func TestSetEdgegridEnvs(t *testing.T) {
 				assert.Equal(t, v, os.Getenv(k))
 			}
 		})
+	}
+}
+
+func TestSetWrongTypeForEdgegridEnvs(t *testing.T) {
+
+	tests := map[string]struct {
+		environmentVars map[string]interface{}
+	}{
+		"nil value for access_token": {
+			environmentVars: map[string]interface{}{"access_token": nil},
+		},
+		"nil value for client_token": {
+			environmentVars: map[string]interface{}{"client_token": nil},
+		},
+		"nil value for host": {
+			environmentVars: map[string]interface{}{"host": nil},
+		},
+		"nil value for client_secret": {
+			environmentVars: map[string]interface{}{"client_secret": nil},
+		},
+		"wrong type of max_body value": {
+			environmentVars: map[string]interface{}{"max_body": "not a number"},
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			err := setEdgegridEnvs(test.environmentVars, "some section")
+			assert.True(t, errors.Is(err, tools.ErrInvalidType))
+		})
+	}
+}
+
+func TestConfigureCache_EnabledInContext(t *testing.T) {
+	tests := map[string]struct {
+		resourceLocalData   *schema.ResourceData
+		expectedMeta        meta
+		expectedDiagnostics diag.Diagnostics
+	}{
+		"cache is enabled": {
+			resourceLocalData: getResourceLocalDataWithBoolValue(t, "cache_enabled", true),
+			expectedMeta: meta{
+				cacheEnabled: true,
+			},
+			expectedDiagnostics: diag.Diagnostics(nil),
+		},
+		"cache is not enabled": {
+			resourceLocalData: getResourceLocalDataWithBoolValue(t, "cache_enabled", false),
+			expectedMeta: meta{
+				cacheEnabled: false,
+			},
+			expectedDiagnostics: diag.Diagnostics(nil),
+		},
+	}
+	for name, test := range tests {
+		ctx := context.Background()
+		t.Run(name, func(t *testing.T) {
+
+			configuredContext, diagnostics := configureContext(ctx, test.resourceLocalData)
+			metaCtx := configuredContext.(*meta)
+
+			assert.Equal(t, test.expectedDiagnostics, diagnostics)
+			assert.Equal(t, test.expectedMeta.cacheEnabled, metaCtx.cacheEnabled)
+			assert.NotEmpty(t, metaCtx.log)
+			assert.NotEmpty(t, metaCtx.operationID)
+			assert.NotEmpty(t, metaCtx.sess)
+		})
+	}
+}
+
+func TestConfigureEdgercInContext(t *testing.T) {
+	tests := map[string]struct {
+		resourceLocalData   *schema.ResourceData
+		expectedDiagnostics diag.Diagnostics
+		withError           bool
+	}{
+		"file with EdgeGrid configuration does not exist": {
+			resourceLocalData:   getResourceLocalData(t, "edgerc", "not_existing_file_path"),
+			expectedDiagnostics: diag.Errorf(ConfigurationIsNotSpecified),
+			withError:           true,
+		},
+		"with empty edgerc path, default path is used": {
+			resourceLocalData:   getResourceLocalData(t, "edgerc", ""),
+			expectedDiagnostics: diag.Diagnostics(nil),
+			withError:           false,
+		},
+	}
+	for name, test := range tests {
+		ctx := context.Background()
+		t.Run(name, func(t *testing.T) {
+			meta, diagnostics := configureContext(ctx, test.resourceLocalData)
+			if test.withError {
+				assert.Nil(t, meta)
+			} else {
+				assert.NotEmpty(t, meta)
+			}
+			assert.Equal(t, diagnostics, test.expectedDiagnostics)
+		})
+	}
+}
+
+func getResourceLocalDataWithBoolValue(t *testing.T, key string, value bool) *schema.ResourceData {
+	resourceSchema := map[string]*schema.Schema{
+		key: {
+			Type: schema.TypeBool,
+		},
+	}
+	resourceDataMap := map[string]interface{}{
+		key: value,
+	}
+	return schema.TestResourceDataRaw(t, resourceSchema, resourceDataMap)
+}
+
+func getResourceLocalData(t *testing.T, key string, value interface{}) *schema.ResourceData {
+	resourceSchema := map[string]*schema.Schema{
+		"cache_enabled": {
+			Type: schema.TypeBool,
+		},
+		key: {
+			Type: schema.TypeString,
+		},
+	}
+
+	dataMap := map[string]interface{}{
+		"cache_enabled": true,
+		key:             value,
+	}
+	return schema.TestResourceDataRaw(t, resourceSchema, dataMap)
+}
+
+func TestGetEdgercPath(t *testing.T) {
+	tests := map[string]struct {
+		edgercPath         string
+		expectedEdgercPath string
+	}{
+		"empty edgerc path": {
+			edgercPath:         "",
+			expectedEdgercPath: edgegrid.DefaultConfigFile,
+		},
+		"existing edgerc path": {
+			edgercPath:         "../.edgerc",
+			expectedEdgercPath: "../.edgerc",
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			edgercPath := getEdgercPath(test.edgercPath)
+			assert.Equal(t, test.expectedEdgercPath, edgercPath)
+		})
+	}
+}
+
+func TestEdgercValidate(t *testing.T) {
+	ctx := context.Background()
+	resourceSchema := map[string]*schema.Schema{
+		"cache_enabled": {
+			Type: schema.TypeBool,
+		},
+		"edgerc": {
+			Type: schema.TypeString,
+		},
+		"config_section": {
+			Type: schema.TypeString,
+		},
+	}
+
+	resourceDataMap := map[string]interface{}{
+		"cache_enabled":  true,
+		"edgerc":         "testdata/edgerc",
+		"config_section": "validate_edgerc",
+	}
+
+	wrongHostFromFile := "\"akaa-ay3i6htctb4uuahh-tklu4vvwja5wzytu.luna-dev.akamaiapis.net/\""
+
+	resourceData := schema.TestResourceDataRaw(t, resourceSchema, resourceDataMap)
+	configuredContext, diagnostics := configureContext(ctx, resourceData)
+
+	assert.Nil(t, configuredContext)
+	assert.Contains(t, diagnostics[0].Summary, wrongHostFromFile)
+}
+
+func Test_mergeSchema(t *testing.T) {
+	tests := map[string]struct {
+		from              map[string]*schema.Schema
+		to                map[string]*schema.Schema
+		expectedSchemaMap map[string]*schema.Schema
+		errorExpected     bool
+	}{
+		"both schemas are the same": {
+			from:          getFirstSchema(),
+			to:            getFirstSchema(),
+			errorExpected: true,
+		},
+		"two different schemas": {
+			from: getFirstSchema(),
+			to:   getSecondSchema(),
+			expectedSchemaMap: map[string]*schema.Schema{
+				"test element": {
+					Optional:    true,
+					Type:        schema.TypeString,
+					Description: "element with type string",
+				},
+				"different test element": {
+					Optional:    true,
+					Type:        schema.TypeBool,
+					Description: "element with type bool",
+				},
+			},
+			errorExpected: false,
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			mergedSchema, err := mergeSchema(test.from, test.to)
+			if test.errorExpected {
+				assert.Nil(t, mergedSchema)
+				assert.True(t, errors.Is(err, ErrDuplicateSchemaKey))
+			} else {
+				assert.Nil(t, err)
+				assert.Equal(t, test.expectedSchemaMap, mergedSchema)
+			}
+		})
+	}
+}
+
+func Test_mergeResource(t *testing.T) {
+	tests := map[string]struct {
+		from                map[string]*schema.Resource
+		to                  map[string]*schema.Resource
+		expectedResourceMap map[string]*schema.Resource
+		errorExpected       bool
+	}{
+		"both resources are the same": {
+			from: map[string]*schema.Resource{
+				"some resource": {
+					Schema: getFirstSchema(),
+				},
+			},
+			to: map[string]*schema.Resource{
+				"some resource": {
+					Schema: getFirstSchema(),
+				},
+			},
+			errorExpected: true,
+		},
+		"two different resources": {
+			from: map[string]*schema.Resource{
+				"first resource": {
+					Schema: getFirstSchema(),
+				},
+			},
+			to: map[string]*schema.Resource{
+				"second resource": {
+					Schema: getSecondSchema(),
+				},
+			},
+			expectedResourceMap: map[string]*schema.Resource{
+				"first resource": {
+					Schema: getFirstSchema(),
+				},
+				"second resource": {
+					Schema: getSecondSchema(),
+				},
+			},
+			errorExpected: false,
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			mergedResource, err := mergeResource(test.from, test.to)
+			if test.errorExpected {
+				assert.Nil(t, mergedResource)
+				assert.True(t, errors.Is(err, ErrDuplicateSchemaKey))
+			} else {
+				assert.Nil(t, err)
+				assert.Equal(t, test.expectedResourceMap, mergedResource)
+			}
+		})
+	}
+}
+
+func getFirstSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"test element": {
+			Optional:    true,
+			Type:        schema.TypeString,
+			Description: "element with type string",
+		},
+	}
+}
+
+func getSecondSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"different test element": {
+			Optional:    true,
+			Type:        schema.TypeBool,
+			Description: "element with type bool",
+		},
 	}
 }
