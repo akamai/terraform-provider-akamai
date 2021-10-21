6daff118bcdabfb8fc993359764df34b70625697
Mateusz Jakubiec <mjakubie@akamai.com>
2021-10-21 13:43:05 +0200

    TFP-903 property rule data source: allow user to select rule format

---
+++ pkg/providers/property/testdata/TestDSPropertyRules/with_versioned_rule_format.tf
@@ -0,0 +1,10 @@
+provider "akamai" {
+  edgerc = "~/.edgerc"
+}
+
+data "akamai_property_rules" "rules" {
+  contract_id = "ctr_2"
+  group_id = "grp_2"
+  property_id = "prp_2"
+  rule_format = "v2015-08-17"
+}
