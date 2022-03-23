5ce735e778f61fd4e67e1b978a0e65d62844e7d9
Roberto Lopez Lopez <rlopezlo@akamai.com>
2022-03-23 13:56:55 +0000

    DXE-682 add sample .edgerc credentials

--- build/internal/releaser/goreleaser.bash
+++ build/internal/releaser/goreleaser.bash
@@ -9,18 +9,24 @@ set -e
 SSH_PRV_KEY="$(cat ~/.ssh/id_rsa)"
 SSH_PUB_KEY="$(cat ~/.ssh/id_rsa.pub)"
 SSH_KNOWN_HOSTS="$(cat ~/.ssh/known_hosts)"
-EDGERC="$(cat ~/.edgerc)"
+IMAGE="tfp-releaser"
+CONTAINER="${IMAGE}-container"
 
 clean_up() {
-  echo "cleaning up:"
-  numConts=$( docker container ls | grep -w 'releaser_container' | wc -l )
+  numConts=$( docker container ls | grep -w "${CONTAINER}" | wc -l )
   if [[ $numConts -ne 0 ]]; then
-    docker container rm -f releaser_container
+    docker container rm -f "${CONTAINER}"
+    echo "removed $numConts containers"
+  else
+    echo "no containers to remove"
   fi
 
-  numImages=$(docker image ls | grep -w "releaser"  | wc -l)
-  if [[ "$numImages" -ne 0 ]]; then
-    docker image rm -f releaser
+  numImages=$( docker image ls | grep -w "${IMAGE}" | wc -l )
+  if [[ $numImages -ne 0 ]]; then
+    docker image rm -f "${IMAGE}"
+    echo "removed $numImages images"
+  else
+    echo "no images to remove"
   fi
   return 0
 }
@@ -29,20 +35,18 @@ clean_up
 
 echo "building image:"
 cd build/internal/releaser
-docker build -f Dockerfile -t "releaser" .
+docker build -f Dockerfile -t "${IMAGE}" .
 
 echo "starting container:"
-docker container run --name releaser_container -d -it \
+docker container run --name "${CONTAINER}" -d -it \
   -e SSH_PUB_KEY="${SSH_PUB_KEY}" \
   -e SSH_PRV_KEY="${SSH_PRV_KEY}" \
-  -e SSH_KNOWN_HOSTS="${SSH_KNOWN_HOSTS}" \
-  -e EDGERC="${EDGERC}" "releaser"
+  -e SSH_KNOWN_HOSTS="${SSH_KNOWN_HOSTS}"  "${IMAGE}"
 
 echo "cloning repositories and executing goreleaser:"
-docker exec releaser_container bash -c 'echo "$SSH_KNOWN_HOSTS" > /root/.ssh/known_hosts;
+docker container exec "${CONTAINER}" bash -c 'echo "$SSH_KNOWN_HOSTS" > /root/.ssh/known_hosts;
           echo "$SSH_PUB_KEY" > /root/.ssh/id_rsa.pub;
           echo "$SSH_PRV_KEY" > /root/.ssh/id_rsa;
-          echo "$EDGERC" > /root/.edgerc;
           chmod 700 /root/.ssh;
           chmod 600 /root/.ssh/id_rsa;
           chmod 644 /root/.ssh/id_rsa.pub;
@@ -56,7 +60,7 @@ docker exec releaser_container bash -c 'echo "$SSH_KNOWN_HOSTS" > /root/.ssh/kno
           goreleaser build --single-target --skip-validate --config ./.goreleaser.yml --output /root/.terraform.d/plugins/registry.terraform.io/akamai/akamai/10.0.0/linux_amd64/terraform-provider-akamai_v10.0.0'
 
 echo "smoke test:"
-docker exec releaser_container bash -c 'cd /workspace/terraform-provider-akamai/examples/akamai_cp_code;
+docker container exec ${CONTAINER} bash -c 'cd /workspace/terraform-provider-akamai/examples/akamai_cp_code;
           terraform init;
           terraform plan'
 
