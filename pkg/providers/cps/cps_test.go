7ba816f5b0f4f77ad778c97f0d4219d4450537b0
Zagrajczuk, Wojciech <wzagrajc@akamai.com>
2022-09-01 15:20:40 +0200

    DXE-1473 DXE-1479 Add missing methods in mockcps

--- pkg/providers/cps/cps_test.go
+++ pkg/providers/cps/cps_test.go
@@ -122,3 +122,123 @@ func (m *mockcps) AcknowledgePreVerificationWarnings(ctx context.Context, r cps.
 
 	return args.Error(0)
 }
+
+func (m *mockcps) GetChangeManagementInfo(ctx context.Context, r cps.GetChangeRequest) (*cps.ChangeManagementInfoResponse, error) {
+	args := m.Called(ctx, r)
+
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(*cps.ChangeManagementInfoResponse), args.Error(1)
+}
+
+func (m *mockcps) GetChangeDeploymentInfo(ctx context.Context, r cps.GetChangeRequest) (*cps.ChangeDeploymentInfoResponse, error) {
+	args := m.Called(ctx, r)
+
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(*cps.ChangeDeploymentInfoResponse), args.Error(1)
+}
+
+func (m *mockcps) ListDeployments(ctx context.Context, r cps.ListDeploymentsRequest) (*cps.ListDeploymentsResponse, error) {
+	args := m.Called(ctx, r)
+
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(*cps.ListDeploymentsResponse), args.Error(1)
+}
+
+func (m *mockcps) GetProductionDeployment(ctx context.Context, r cps.GetDeploymentRequest) (*cps.GetProductionDeploymentResponse, error) {
+	args := m.Called(ctx, r)
+
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(*cps.GetProductionDeploymentResponse), args.Error(1)
+}
+
+func (m *mockcps) GetStagingDeployment(ctx context.Context, r cps.GetDeploymentRequest) (*cps.GetStagingDeploymentResponse, error) {
+	args := m.Called(ctx, r)
+
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(*cps.GetStagingDeploymentResponse), args.Error(1)
+}
+
+func (m *mockcps) GetDeploymentSchedule(ctx context.Context, r cps.GetDeploymentScheduleRequest) (*cps.DeploymentSchedule, error) {
+	args := m.Called(ctx, r)
+
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(*cps.DeploymentSchedule), args.Error(1)
+}
+
+func (m *mockcps) UpdateDeploymentSchedule(ctx context.Context, r cps.UpdateDeploymentScheduleRequest) (*cps.UpdateDeploymentScheduleResponse, error) {
+	args := m.Called(ctx, r)
+
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(*cps.UpdateDeploymentScheduleResponse), args.Error(1)
+}
+
+func (m *mockcps) GetDVHistory(ctx context.Context, r cps.GetDVHistoryRequest) (*cps.GetDVHistoryResponse, error) {
+	args := m.Called(ctx, r)
+
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(*cps.GetDVHistoryResponse), args.Error(1)
+}
+
+func (m *mockcps) GetCertificateHistory(ctx context.Context, r cps.GetCertificateHistoryRequest) (*cps.GetCertificateHistoryResponse, error) {
+	args := m.Called(ctx, r)
+
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(*cps.GetCertificateHistoryResponse), args.Error(1)
+}
+
+func (m *mockcps) GetChangeHistory(ctx context.Context, r cps.GetChangeHistoryRequest) (*cps.GetChangeHistoryResponse, error) {
+	args := m.Called(ctx, r)
+
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(*cps.GetChangeHistoryResponse), args.Error(1)
+}
+
+func (m *mockcps) GetChangePostVerificationWarnings(ctx context.Context, r cps.GetChangeRequest) (*cps.PostVerificationWarnings, error) {
+	args := m.Called(ctx, r)
+
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(*cps.PostVerificationWarnings), args.Error(1)
+}
+
+func (m *mockcps) GetChangeThirdPartyCSR(ctx context.Context, r cps.GetChangeRequest) (*cps.ThirdPartyCSRResponse, error) {
+	args := m.Called(ctx, r)
+
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(*cps.ThirdPartyCSRResponse), args.Error(1)
+}
