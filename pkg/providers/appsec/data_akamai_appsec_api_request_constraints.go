a289e1787c43319c533d2d06c0b107daf443378e
Michal Wojcik <miwojci@akamai.com>
2022-11-07 11:30:46 +0100

    DXE-1762 Correct name of dependency

--- pkg/providers/appsec/data_akamai_appsec_api_request_constraints.go
+++ pkg/providers/appsec/data_akamai_appsec_api_request_constraints.go
@@ -6,7 +6,7 @@ import (
 	"errors"
 	"strconv"
 
-	v2 "github.com/akamai/AkamaiOPEN-edgegrid-golang/v2/pkg/appsec"
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v2/pkg/appsec"
 	"github.com/akamai/terraform-provider-akamai/v2/pkg/akamai"
 	"github.com/akamai/terraform-provider-akamai/v2/pkg/tools"
 
@@ -52,7 +52,7 @@ func dataSourceAPIRequestConstraintsRead(ctx context.Context, d *schema.Resource
 	client := inst.Client(meta)
 	logger := meta.Log("APPSEC", "dataSourceAPIRequestConstraintsRead")
 
-	getAPIiRequestConstraints := v2.GetApiRequestConstraintsRequest{}
+	getAPIiRequestConstraints := appsec.GetApiRequestConstraintsRequest{}
 
 	configID, err := tools.GetIntValue("config_id", d)
 	if err != nil {
