2c24b6906717966db9b20de0c692d216c6a9ae2d
Dawid Dzhafarov <ddzhafar@akamai.com>
2022-12-06 17:15:04 +0000

    DXE-1688 Fix build issue in akamai_cp_code resource

--- pkg/providers/property/data_akamai_cp_code.go
+++ pkg/providers/property/data_akamai_cp_code.go
@@ -133,5 +133,5 @@ func findCPCode(ctx context.Context, nameOrID, contractID, groupID string, meta 
 		}
 	}
 
-	return nil, fmt.Errorf("%v: CP code: %s", ErrCpCodeNotFound, nameOrID)
+	return nil, fmt.Errorf("%w: CP code: %s", ErrCpCodeNotFound, nameOrID)
 }
