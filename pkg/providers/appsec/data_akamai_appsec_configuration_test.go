c2e7ea8ea1ffce45dfb8d1df60abc5b1df95aeaf
James Lodine <plodine@akamai.com>
2022-12-07 17:58:34 +0000

    SECKSD-17314 Fix error message when nonexistent config specified

--- pkg/providers/appsec/data_akamai_appsec_configuration_test.go
+++ pkg/providers/appsec/data_akamai_appsec_configuration_test.go
@@ -2,6 +2,8 @@ package appsec
 
 import (
 	"encoding/json"
+	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v3/pkg/appsec"
@@ -41,4 +43,36 @@ func TestAkamaiConfiguration_data_basic(t *testing.T) {
 		client.AssertExpectations(t)
 	})
 
+}
+
+func TestAkamaiConfiguration_data_nonexistentConfig(t *testing.T) {
+	t.Run("nonexistent configuration", func(t *testing.T) {
+		client := &appsec.Mock{}
+
+		getConfigurationsResponse := appsec.GetConfigurationsResponse{}
+		err := json.Unmarshal(loadFixtureBytes("testdata/TestDSConfiguration/Configuration.json"), &getConfigurationsResponse)
+		require.NoError(t, err)
+
+		client.On("GetConfigurations",
+			mock.Anything,
+			appsec.GetConfigurationsRequest{},
+		).Return(nil, fmt.Errorf("configuration 'Nonexistent' not found"))
+		useClient(client, func() {
+			resource.Test(t, resource.TestCase{
+				IsUnitTest: true,
+				Providers:  testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config: loadFixtureString("testdata/TestDSConfiguration/nonexistent_config.tf"),
+						Check: resource.ComposeAggregateTestCheckFunc(
+							resource.TestCheckResourceAttr("data.akamai_appsec_configuration.test", "id", "43253"),
+						),
+						ExpectError: regexp.MustCompile(`configuration 'Nonexistent' not found`),
+					},
+				},
+			})
+		})
+
+		client.AssertExpectations(t)
+	})
 }
