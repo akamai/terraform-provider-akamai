8f8c0e5461acfff8a63433740e45bfc9a7622728
Michal Mazur <mmazur@akamai.com>
2023-04-06 11:36:39 +0000

    DXE-1520 delete product id from import hostname

--- pkg/providers/property/testdata/TestResourceEdgeHostname/creation_before_import_edgehostname.tf
+++ pkg/providers/property/testdata/TestResourceEdgeHostname/creation_before_import_edgehostname.tf
@@ -2,9 +2,10 @@ provider "akamai" {
   edgerc = "../../test/edgerc"
 }
 
-resource "akamai_edge_hostname" "importedgehostname" {
+resource "akamai_edge_hostname" "createhostname" {
   contract      = "ctr_1"
   group         = "grp_2"
+  product       = "prd_2"
   edge_hostname = "test.akamaized.net"
   ip_behavior   = "IPV4"
 }
---
+++ pkg/providers/property/testdata/TestResourceEdgeHostname/new_akamaized_net_without_product_id.tf
@@ -0,0 +1,14 @@
+provider "akamai" {
+  edgerc = "../../test/edgerc"
+}
+
+resource "akamai_edge_hostname" "edgehostname" {
+  contract      = "ctr_2"
+  group         = "grp_2"
+  edge_hostname = "test.akamaized.net"
+  ip_behavior   = "IPV6_COMPLIANCE"
+}
+
+output "edge_hostname" {
+  value = akamai_edge_hostname.edgehostname.edge_hostname
+}
