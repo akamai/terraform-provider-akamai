19ab44fc54b94395b61432cf693beca28371278f
Michal Wojcik <miwojci@akamai.com>
2024-09-03 16:00:43 +0200

    DXE-4183 Revert handle retries for 429 code with X-RateLimit-Next
    This reverts commit 161c2ad4c7b1ce76a72404bfd0d77b0aec01e62d.

--- internal/test/test.go
+++ internal/test/test.go
@@ -2,13 +2,9 @@
 package test
 
 import (
-	"math/rand"
-	"net/http"
-	"sync"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -18,89 +14,4 @@ func NewTimeFromString(t *testing.T, s string) time.Time {
 	parsedTime, err := time.Parse(time.RFC3339Nano, s)
 	require.NoError(t, err)
 	return parsedTime
-}
-
-// XRateLimitHTTPHandler first returns status 429 with the X-RateLimit-Next header set to
-// time.Now() plus a random value between 1 and 5 seconds. It keeps sending 429 until the
-// X-RateLimit-Next point in time. Then it starts to return SuccessCode and SuccessBody
-// indefinitely.
-type XRateLimitHTTPHandler struct {
-	T           *testing.T
-	SuccessCode int
-	SuccessBody string
-
-	mutex         sync.Mutex
-	availableAt   time.Time
-	returnedCodes []int
-	returnTimes   []time.Time
-}
-
-func (h *XRateLimitHTTPHandler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
-	av := h.AvailableAt()
-
-	if av.IsZero() {
-		busyInterval := time.Duration(1+rand.Intn(4)) * time.Second
-		h.setAvailableAt(time.Now().Add(busyInterval))
-		h.setTooManyRequests(w)
-		return
-	}
-
-	now := time.Now()
-	if now.Before(av) {
-		h.setTooManyRequests(w)
-	} else {
-		h.setStatusCode(w, h.SuccessCode)
-		_, err := w.Write([]byte(h.SuccessBody))
-		assert.NoError(h.T, err)
-	}
-}
-
-// AvailableAt returns the point in time at which the handler stops returning status code 429
-func (h *XRateLimitHTTPHandler) AvailableAt() time.Time {
-	h.mutex.Lock()
-	defer h.mutex.Unlock()
-	return h.availableAt
-}
-
-// ReturnedCodes returns a list of status codes from subsequent handler responses
-func (h *XRateLimitHTTPHandler) ReturnedCodes() []int {
-	h.mutex.Lock()
-	defer h.mutex.Unlock()
-	res := make([]int, len(h.returnedCodes))
-	copy(res, h.returnedCodes)
-	return res
-}
-
-// ReturnTimes returns a list of times at which subsequent responses were written
-func (h *XRateLimitHTTPHandler) ReturnTimes() []time.Time {
-	h.mutex.Lock()
-	defer h.mutex.Unlock()
-	res := make([]time.Time, len(h.returnTimes))
-	copy(res, h.returnTimes)
-	return res
-}
-
-func (h *XRateLimitHTTPHandler) setTooManyRequests(w http.ResponseWriter) {
-	// Do not use Add() to avoid canonicalization to X-Ratelimit-Next
-	nextStr := h.availableAt.Format(time.RFC3339Nano)
-	w.Header()["X-RateLimit-Next"] = []string{nextStr}
-	h.setStatusCode(w, http.StatusTooManyRequests)
-	body := "Your request did not succeed as this operation has reached the limit " +
-		"for your account. Please try after " + nextStr
-	_, err := w.Write([]byte(body))
-	assert.NoError(h.T, err)
-}
-
-func (h *XRateLimitHTTPHandler) setStatusCode(w http.ResponseWriter, statusCode int) {
-	w.WriteHeader(statusCode)
-	h.mutex.Lock()
-	defer h.mutex.Unlock()
-	h.returnedCodes = append(h.returnedCodes, statusCode)
-	h.returnTimes = append(h.returnTimes, time.Now())
-}
-
-func (h *XRateLimitHTTPHandler) setAvailableAt(availableAt time.Time) {
-	h.mutex.Lock()
-	defer h.mutex.Unlock()
-	h.availableAt = availableAt
 }
