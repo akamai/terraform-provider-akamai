abb655e9030d1d22d60afccaefdd9fa47699030d
Zagrajczuk, Wojciech <wzagrajc@akamai.com>
2025-05-26 16:03:37 +0200

    DXE-5104 Fix goreleaser

--- build/internal/releaser/goreleaser_build.bash
+++ build/internal/releaser/goreleaser_build.bash
@@ -1,4 +1,4 @@
 cd /workspace/terraform-provider-akamai
-go mod edit -replace github.com/akamai/AkamaiOPEN-edgegrid-golang/v11=../akamaiopen-edgegrid-golang/
+#go mod edit -replace github.com/akamai/AkamaiOPEN-edgegrid-golang/v11=../akamaiopen-edgegrid-golang/
 git tag v10.0.0
 goreleaser build --single-target --skip=validate --config ./.goreleaser.yml --output /root/.terraform.d/plugins/registry.terraform.io/akamai/akamai/10.0.0/linux_amd64/terraform-provider-akamai_v10.0.0
