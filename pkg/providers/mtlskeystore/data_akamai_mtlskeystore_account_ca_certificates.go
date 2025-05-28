32307a0bd9b79ebe7ce0aa59540b14bff9029d26
Zagrajczuk, Wojciech <wzagrajc@akamai.com>
2025-05-28 11:33:45 +0200

    DXE-5104 Fix build after major release

--- pkg/providers/mtlskeystore/data_akamai_mtlskeystore_account_ca_certificates.go
+++ pkg/providers/mtlskeystore/data_akamai_mtlskeystore_account_ca_certificates.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v10/pkg/mtlskeystore"
-	"github.com/akamai/terraform-provider-akamai/v7/pkg/meta"
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v11/pkg/mtlskeystore"
+	"github.com/akamai/terraform-provider-akamai/v8/pkg/meta"
 	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
