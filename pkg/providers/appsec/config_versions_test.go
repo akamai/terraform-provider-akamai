af2d129bb80ebb11db3ae07262f3f5f698ad0209
Wiktor Wilkusz <wwilkusz@akamai.com>
2026-02-02 11:19:09 +0000

    DXE-5751 Bring back the breaking changes in appsec

--- pkg/providers/appsec/config_versions_test.go
+++ pkg/providers/appsec/config_versions_test.go
@@ -391,17 +391,11 @@ func TestGetModifiableConfigVersion_LatestVersionIsModifiable(t *testing.T) {
 		BasedOn:    1,
 		CreateDate: time.Now(),
 		CreatedBy:  "test@example.com",
-		Production: struct {
-			Status string    `json:"status,omitempty"`
-			Time   time.Time `json:"time,omitempty"`
-		}{
+		Production: appsec.EnvironmentStatus{
 			Status: "Inactive",
 			Time:   time.Now(),
 		},
-		Staging: struct {
-			Status string    `json:"status,omitempty"`
-			Time   time.Time `json:"time,omitempty"`
-		}{
+		Staging: appsec.EnvironmentStatus{
 			Status: "Inactive",
 			Time:   time.Now(),
 		},
@@ -575,17 +569,11 @@ func TestGetModifiableConfigVersion_LatestVersionWasPreviouslyActive(t *testing.
 		BasedOn:    1,
 		CreateDate: time.Now(),
 		CreatedBy:  "test@example.com",
-		Production: struct {
-			Status string    `json:"status,omitempty"`
-			Time   time.Time `json:"time,omitempty"`
-		}{
+		Production: appsec.EnvironmentStatus{
 			Status: "Inactive",
 			Time:   time.Now(),
 		},
-		Staging: struct {
-			Status string    `json:"status,omitempty"`
-			Time   time.Time `json:"time,omitempty"`
-		}{
+		Staging: appsec.EnvironmentStatus{
 			Status: "Deactivated", // Was previously active
 			Time:   time.Now(),
 		},
@@ -744,17 +732,11 @@ func TestGetModifiableConfigVersion_ConcurrentAccess(t *testing.T) {
 		BasedOn:    1,
 		CreateDate: time.Now(),
 		CreatedBy:  "test@example.com",
-		Production: struct {
-			Status string    `json:"status,omitempty"`
-			Time   time.Time `json:"time,omitempty"`
-		}{
+		Production: appsec.EnvironmentStatus{
 			Status: "Inactive",
 			Time:   time.Now(),
 		},
-		Staging: struct {
-			Status string    `json:"status,omitempty"`
-			Time   time.Time `json:"time,omitempty"`
-		}{
+		Staging: appsec.EnvironmentStatus{
 			Status: "Inactive",
 			Time:   time.Now(),
 		},
