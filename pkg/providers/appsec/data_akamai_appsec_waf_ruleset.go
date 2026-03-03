5b588e41f374d18cb68c6526216e5b1b752248af
Tatiana Slonimskaia <tslonims@akamai.com>
2026-03-03 09:12:00 +0000

    DXE-6207 Updated major edgegrid and terraform versions after release

--- pkg/providers/appsec/data_akamai_appsec_waf_ruleset.go
+++ pkg/providers/appsec/data_akamai_appsec_waf_ruleset.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"sort"
 
-	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v12/pkg/appsec"
-	"github.com/akamai/terraform-provider-akamai/v9/pkg/meta"
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v13/pkg/appsec"
+	"github.com/akamai/terraform-provider-akamai/v10/pkg/meta"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
